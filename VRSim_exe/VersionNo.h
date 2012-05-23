30474318ccf129a945359284e4b8a53da5c49b98
MaximSter <15948862+DieSlower@users.noreply.github.com>
2012-05-22 22:17:14 -0400

    Implemented a temporary GPU detection for X11 Systems. It does not really detect GPUs but returns a Screen Structure which will always force each window to have its own context. This will need to be changed if VRSim will be used on linux

--- VRSim_exe/VersionNo.h
+++ VRSim_exe/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER					1,0,2,0348
-//Do Not Remove				1,0,0,1972
-#define STRFILEVER				"1,0,2,0348"
-//Do Not Remove			"1,0,0,1972"
+#define FILEVER					1,0,2,0350
+//Do Not Remove				1,0,0,1974
+#define STRFILEVER				"1,0,2,0350"
+//Do Not Remove			"1,0,0,1974"
--- vrsPicatinny_DLL/VersionNo.h
+++ vrsPicatinny_DLL/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER					1,0,0,1041
-//Do Not Remove				1,0,0,1041
-#define STRFILEVER				"1,0,0,1041"
-//Do Not Remove			"1,0,0,1041"
+#define FILEVER					1,0,0,1043
+//Do Not Remove				1,0,0,1043
+#define STRFILEVER				"1,0,0,1043"
+//Do Not Remove			"1,0,0,1043"
