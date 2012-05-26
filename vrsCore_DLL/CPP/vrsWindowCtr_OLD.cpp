1581aebf44f250b97c2b1ff59bf7cf4caf737da5
MaximSter <15948862+DieSlower@users.noreply.github.com>
2012-05-26 19:31:15 -0400

    Renamed the Old Window Controller to vrsWindowCtr_OLD

--- vrsCore_DLL/CPP/vrsWindowCtr_OLD.cpp
+++ vrsCore_DLL/CPP/vrsWindowCtr_OLD.cpp
@@ -2,7 +2,7 @@
 #ifndef vrsWindow_CPP
 #define vrsWindow_CPP
 
-#include "..\\Include\vrsWindowCtr.h"
+#include "..\\Include\vrsWindowCtr_OLD.h"
 
 vrsWindowCtr::vrsWindowCtr(void):
 			//_Window(0), 
