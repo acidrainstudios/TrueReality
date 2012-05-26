b70dcb25dcc79d175674a151181eadcea402a5f3
MaximSter <MaximSter@localhost>
2012-05-26 19:29:10 -0400

    Added the vrsConfigParser class. It will parse the main VRSImConfig file to store and get various configuration options. Fixed a spelling mistake in vrsWindowCtr on a comment line

---
+++ vrsUtil_DLL/CPP/vrsConfigParser.cpp
@@ -0,0 +1,31 @@
+/*
+* //////////////////////////////////////C++//////////////////////////////////////
+* 
+* Virtual Reality Simulation Lab
+* 
+* This file is part of Virtual Reality Simulation Lab software. You can redistribute 
+* it and/or modify it only with the authors permission. All unlicensed use and modification
+* of this file are prohibited. 
+* 
+* This does not restrict the user from creating custom modules and additional files to 
+* interface with the VR-Sim Lab.
+* 
+* This software was developed by Maxim Serebrennik for Various projects he worked on.
+* It is sold and distributed only with his permission. 
+* 
+* For more information e-mail him at MaximSter@gmail.com
+* 
+* @author Maxim Serebrennik 
+*////////////////////////////////////////////////////////////////////////////////
+
+#ifndef vrsConfigParser_CPP
+#define vrsConfigParser_CPP 1
+
+#include "..\\Include\vrsConfigParser.h"
+
+namespace vrsUtil
+{
+
+}
+
+#endif // vrsConfigParser_CPP
