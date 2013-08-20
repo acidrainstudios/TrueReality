4947594d594fe268e0083cea7d70555a31e3b0ff
DieSlower <15948862+DieSlower@users.noreply.github.com>
2013-08-20 00:45:55 -0400

    Renames the vrsUtil folders.

--- VRSim_exe/VersionNo.h
+++ VRSim_exe/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER					1,0,2,0419
-//Do Not Remove				1,0,0,2043
-#define STRFILEVER				"1,0,2,0419"
-//Do Not Remove			"1,0,0,2043"
+#define FILEVER					1,0,2,0423
+//Do Not Remove				1,0,0,2047
+#define STRFILEVER				"1,0,2,0423"
+//Do Not Remove			"1,0,0,2047"
