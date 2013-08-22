5d9c33eee4ca8de41d196284f31d83c71c2a9b94
DieSlower <maximster@gmail.com>
2013-08-21 21:13:21 -0400

    Removing version version files while doing the restructuring...

--- AutoBuildNumber/VersionInf.h
+++
@@ -1,8 +0,0 @@
-#include "..\\vrsVersionCTR\ProdVerInf.h"
-#include "VersionNo.h"
-
-#define	STRComments				"TheCore2.dll Is The Main Controller for VR-Sim Lab\0"
-#define STROriginalFilename		"TheCore2.dll\0"
-#define STRFileDescription		"TheCore2.dll Is The Main Controller for VR-Sim Lab\0"
-            
-
