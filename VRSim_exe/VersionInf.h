341077dc0a52ed6ec935cb46304b9268ef1f2e73
DieSlower <maximster@gmail.com>
2013-08-21 21:26:13 -0400

    Removing unused files.

--- VRSim_exe/VersionInf.h
+++
@@ -1,7 +0,0 @@
-#include "VersionNo.h"
-
-#define	STRComments				"Virtual Reality Simulation Lab Is A Program For Industrial, Military, And Scientific Simulation \0"
-#define STROriginalFilename		"VRSim.exe\0"
-#define STRFileDescription		"VRSim.exe Is The Initialization Point\0"
-            
-
--- vrsUtil_DLL/VersionInf.h
+++
@@ -1,7 +0,0 @@
-#include "VersionNo.h"
-
-#define	STRComments				"vrsUFVG.dll Is The Controller For The Underwater Flow Vector Generators\0"
-#define STROriginalFilename		"vrsUFVG.dll\0"
-#define STRFileDescription		"Underwater Flow Vector Generator\0"
-            
-
