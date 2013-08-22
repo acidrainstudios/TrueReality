7fa8c471f1267f9bdec756312406cab07fc49d59
DieSlower <maximster@gmail.com>
2013-08-21 21:03:32 -0400

    Moving cpp files into a new folder structure for VRSim exe

--- VRSim_exe/VersionNo.h
+++ VRSim_exe/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER					1,0,2,0440
-//Do Not Remove				1,0,0,2064
-#define STRFILEVER				"1,0,2,0440"
-//Do Not Remove			"1,0,0,2064"
+#define FILEVER					1,0,2,0441
+//Do Not Remove				1,0,0,2065
+#define STRFILEVER				"1,0,2,0441"
+//Do Not Remove			"1,0,0,2065"
--- vrsUtil_DLL/VersionNo.h
+++ vrsUtil_DLL/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER					1,3,0,2357
-//Do Not Remove				1,0,0,2274
-#define STRFILEVER				"1,3,0,2357"
-//Do Not Remove			"1,0,0,2274"
+#define FILEVER					1,3,0,2358
+//Do Not Remove				1,0,0,2275
+#define STRFILEVER				"1,3,0,2358"
+//Do Not Remove			"1,0,0,2275"
