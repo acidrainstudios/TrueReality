f54d705d2ea1accbfb765cd927f3fb1404c7b7d0
DieSlower <15948862+DieSlower@users.noreply.github.com>
2013-08-21 21:47:09 -0400

    Updates vrsCore dll headers

--- src/vrsCore/vrsCoreDLLEntryPoint.cpp
+++ src/vrsCore/vrsCoreDLLEntryPoint.cpp
@@ -1,22 +1,24 @@
 /*
-* //////////////////////////////////////C++//////////////////////////////////////
-* 
-* Virtual Reality Simulation Lab
-* 
-* This file is part of Virtual Reality Simulation Lab software. You can redistribute 
-* it and/or modify it only with the authors permission. All unlicensed use and modification
-* of this file are prohibited. 
-* 
-* This does not restrict the user from creating custom modules and additional files to 
-* interface with the VR-Sim Lab.
-* 
-* This software was developed by Maxim Serebrennik for Various projects he worked on.
-* It is sold and distributed only with his permission. 
-* 
-* For more information e-mail him at MaximSter@gmail.com
-* 
-* @author Maxim Serebrennik 
-*////////////////////////////////////////////////////////////////////////////////
+* The Construct Open Source Game and Simulation Engine
+* Copyright (C) 2013
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+
 
 #ifndef COREDLL_MY_GAME_ENTRY_POINT_CPP
 #define COREDLL_MY_GAME_ENTRY_POINT_CPP
