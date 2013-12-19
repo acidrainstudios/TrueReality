b6c89725134d538df8de51fd77da14fffb3b3084
DieSlower <15948862+DieSlower@users.noreply.github.com>
2013-12-18 23:16:27 -0400

    Adding Delta3D search paths to the scripts.

--- CMakeModules/FindOpenThreads.cmake
+++ CMakeModules/FindOpenThreads.cmake
@@ -1,6 +1,12 @@
 # Finds the include files necessary for compilation
 find_path (OPENTHREADS_INCLUDE_DIR OpenThreads/Thread
     HINTS
+	$ENV{DELTA_INC} 
+	$ENV{DELTA_INC}
+    $ENV{DELTA_ROOT}/Ext/include
+    $ENV{DELTA_ROOT}/Ext
+    $ENV{DELTA_ROOT}/include
+    $ENV{DELTA_ROOT}   
     $ENV{TR_INC}
     $ENV{TR_ROOT}/Ext/include
     $ENV{TR_ROOT}/Ext
@@ -18,35 +24,40 @@ mark_as_advanced (OPENTHREADS_INCLUDE_DIR)
 macro (FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
     # Finds the library files necessary for compilation
     find_library (${MYLIBRARY}
-        NAMES ${MYLIBRARYNAME}
-        HINTS
-        $ENV{TR_ROOT}/Ext/lib
-        $ENV{TR_ROOT}/Ext
-        $ENV{TR_LIB}
-        $ENV{TR_ROOT}/lib64
-        $ENV{TR_ROOT}/lib
-        $ENV{TR_ROOT}
-        $ENV{OPENTHREADS_LIB}
-        $ENV{OPENTHREADS_ROOT}/lib64
-        $ENV{OPENTHREADS_ROOT}/lib
-        $ENV{OPENTHREADS_ROOT}
-        $ENV{OSG_INC}
-        $ENV{OSG_ROOT}/lib64
-        $ENV{OSG_ROOT}/lib
-        $ENV{OSG_ROOT}
-    )
+    NAMES ${MYLIBRARYNAME}
+    HINTS
+	$ENV{DELTA_LIB}
+	$ENV{DELTA_ROOT}/Ext/lib
+    $ENV{DELTA_ROOT}/Ext
+    $ENV{DELTA_ROOT}/lib64
+    $ENV{DELTA_ROOT}/lib
+    $ENV{DELTA_ROOT}
+    $ENV{TR_ROOT}/Ext/lib
+    $ENV{TR_ROOT}/Ext
+    $ENV{TR_ROOT}/lib64
+    $ENV{TR_ROOT}/lib
+    $ENV{TR_ROOT}
+    $ENV{OPENTHREADS_LIB}
+    $ENV{OPENTHREADS_ROOT}/lib64
+    $ENV{OPENTHREADS_ROOT}/lib
+    $ENV{OPENTHREADS_ROOT}
+    $ENV{OSG_INC}
+    $ENV{OSG_ROOT}/lib64
+    $ENV{OSG_ROOT}/lib
+    $ENV{OSG_ROOT}
+)
     mark_as_advanced (${MYLIBRARY})
 endmacro(FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
 
-if (TC_BUILD_WITH_RELEASE)
+if (TR_BUILD_WITH_RELEASE)
     # Locates all OPENTHREADS release libraries
     FIND_OPENTHREADS_LIBRARY (OPENTHREADS_LIBRARY OpenThreads)
-endif (TC_BUILD_WITH_RELEASE)
+endif (TR_BUILD_WITH_RELEASE)
 
-if (TC_BUILD_WITH_DEBUG)
+if (TR_BUILD_WITH_DEBUG)
     # Locates all OPENTHREADS debug libraries
     FIND_OPENTHREADS_LIBRARY (OPENTHREADS_LIBRARY_DEBUG OpenThreadsd)
-endif (TC_BUILD_WITH_DEBUG)
+endif (TR_BUILD_WITH_DEBUG)
 
 set (DEPENDENCY_FOUND "NO")
 if (OPENTHREADS_LIBRARY AND OPENTHREADS_INCLUDE_DIR)
