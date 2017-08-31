46a71eef42d79f3bfea74b5eaaa7dc8a9e415d95
DieSlower <maximster@gmail.com>
2017-08-31 00:11:55 -0400

    Issue #25 Updates the cmake Utility Macros

--- CMakeModules/UtilityMacros.cmake
+++ CMakeModules/UtilityMacros.cmake
@@ -1,28 +1,28 @@
-macro (TR_DETECT_DEPENDENCY arg)
-    message (STATUS "Detecting ${arg}...")
-    find_package (${arg} REQUIRED)
-    if (DEPENDENCY_FOUND)
-        message (STATUS "Found ${arg}!")
-        include_directories(${${arg}_INCLUDE_DIR})
-    else (DEPENDENCY_FOUND)
-        message ("${arg} not found!")
-    endif (DEPENDENCY_FOUND)
-endmacro (TR_DETECT_DEPENDENCY arg)
+MACRO (TR_DETECT_DEPENDENCY arg)
+    MESSAGE (STATUS "Detecting ${arg}...")
+    FIND_PACKAGE (${arg} REQUIRED)
+    IF (DEPENDENCY_FOUND)
+        MESSAGE (STATUS "Found ${arg}!")
+        INCLUDE_DIRECTORIES(${${arg}_INCLUDE_DIR})
+    ELSE (DEPENDENCY_FOUND)
+        MESSAGE ("${arg} not found!")
+    ENDIF (DEPENDENCY_FOUND)
+ENDMACRO (TR_DETECT_DEPENDENCY arg)
 
-macro (TR_TARGET_OPTIONS arg)
-    set_target_properties (
+MACRO (TR_TARGET_OPTIONS arg)
+    SET_TARGET_PROPERTIES (
         ${arg}
         PROPERTIES
         DEBUG_POSTFIX ${CMAKE_DEBUG_POSTFIX}
 		RELWITHDEBINFO_POSTFIX ${CMAKE_RELWITHDEBINFO_POSTFIX}
     )
-    if (MSVC_IDE)
-        target_compile_options (
+    IF (MSVC_IDE)
+        TARGET_COMPILE_OPTIONS (
             ${arg} PRIVATE
         	/nologo /GR
             $<$<CONFIG:Debug>:/Od /Ob0 /RTC1 /D_DEBUG /Zi>
 			$<$<CONFIG:RelWithDebInfo>:/MD /Zi /O2 /Ob1 /D>            
 			$<$<CONFIG:Release>:/MT /O2 /Ob2 /MD>			
         )
-    endif (MSVC_IDE)
-endmacro (TR_TARGET_OPTIONS arg)
+    ENDIF (MSVC_IDE)
+ENDMACRO (TR_TARGET_OPTIONS arg)
