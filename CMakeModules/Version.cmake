2500938df1eab616a6cbf752fd417b62275ad67f
DieSlower <maximster@gmail.com>
2017-08-31 00:28:47 -0400

    Issue #25 Adds a cmake version macro

---
+++ CMakeModules/Version.cmake
@@ -0,0 +1,36 @@
+# *****************************************************************************
+# Displays the current version of True Reality ********************************
+# *****************************************************************************
+# *****************************************************************************
+# Creates and sets version variables to use through out cmake *****************
+# TR_VERSION_MAJOR - the major version number *********************************
+# TR_VERSION_MINOR - the minor version number *********************************
+# TR_VERSION_YYMM - the Year and Month number *********************************
+# TR_VERSION_BUILD - build number *********************************************
+# *****************************************************************************
+# TR_VERSION - The combined version number ************************************
+# TR_SOVERSION - The combined version number for libraries ********************
+# TR_SO_PREFIX - The prefix that is used for all TR Libraries *****************
+# *****************************************************************************
+SET (TR_VERSION_FILE "Data/Config/Version.trver")
+FILE(READ ${TR_VERSION_FILE} TR_VERSION_CONTENT)
+
+IF("${TR_VERSION_CONTENT}" STREQUAL "")
+    MESSAGE("Missing the version file")
+ENDIF()
+
+string (REGEX MATCH "Build\" : ([0-9]+)" TR_VERSION_BUILD ${TR_VERSION_CONTENT})
+string (REGEX MATCH "Major\" : ([0-9]+)" TR_VERSION_MAJOR ${TR_VERSION_CONTENT})
+string (REGEX MATCH "Minor\" : ([0-9]+)" TR_VERSION_MINOR ${TR_VERSION_CONTENT})
+string (REGEX MATCH "YYMM\" : \"([0-9]+)" TR_VERSION_YYMM ${TR_VERSION_CONTENT})
+string (REGEX REPLACE "([^0-9]+)([0-9]+)" "\\2" TR_VERSION_BUILD ${TR_VERSION_BUILD})
+string (REGEX REPLACE "([^0-9]+)([0-9]+)" "\\2" TR_VERSION_MAJOR ${TR_VERSION_MAJOR})
+string (REGEX REPLACE "([^0-9]+)([0-9]+)" "\\2" TR_VERSION_MINOR ${TR_VERSION_MINOR})
+string (REGEX REPLACE "([^0-9]+)([0-9]+)" "\\2" TR_VERSION_YYMM ${TR_VERSION_YYMM})
+
+SET (TR_VERSION ${TR_VERSION_MAJOR}.${TR_VERSION_MINOR}.${TR_VERSION_YYMM}.${TR_VERSION_BUILD})
+SET (TR_SOVERSION ${TR_VERSION_MAJOR}${TR_VERSION_MINOR})
+SET (TR_SO_PREFIX "tr${TR_SOVERSION}-")
+# *****************************************************************************
+# *****************************************************************************
+# *****************************************************************************
