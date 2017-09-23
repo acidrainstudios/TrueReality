13d99aab81e8cef319c9cf19999a8f09dd07ae9f
DieSlower <maximster@gmail.com>
2017-09-23 14:38:58 -0400

    Issue #25 Updates the cmake Open Threads macros.

--- CMakeModules/FindOpenThreads.cmake
+++ CMakeModules/FindOpenThreads.cmake
@@ -1,5 +1,5 @@
 # Finds the include files necessary for compilation
-find_path (OPENTHREADS_INCLUDE_DIR OpenThreads/Thread
+FIND_PATH (OPENTHREADS_INCLUDE_DIR OpenThreads/Thread
     HINTS
     $ENV{TR_INC}
     $ENV{TR_ROOT}/Ext/include
@@ -12,42 +12,56 @@ find_path (OPENTHREADS_INCLUDE_DIR OpenThreads/Thread
     $ENV{OSG_INC}
     $ENV{OSG_ROOT}/include
     $ENV{OSG_ROOT}
+	PATHS
+    /usr/include
+    /usr/local/include
+    ~/Library/Frameworks
+    /Library/Frameworks
 )
-mark_as_advanced (OPENTHREADS_INCLUDE_DIR)
+MARK_AS_ADVANCED (OPENTHREADS_INCLUDE_DIR)
 
-macro (FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
+MACRO (FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
     # Finds the library files necessary for compilation
-    find_library (${MYLIBRARY}
-    NAMES ${MYLIBRARYNAME}
-    HINTS
-    $ENV{TR_ROOT}/Ext/lib
-    $ENV{TR_ROOT}/Ext
-    $ENV{TR_ROOT}/lib64
-    $ENV{TR_ROOT}/lib
-    $ENV{TR_ROOT}
-    $ENV{OPENTHREADS_LIB}
-    $ENV{OPENTHREADS_ROOT}/lib64
-    $ENV{OPENTHREADS_ROOT}/lib
-    $ENV{OPENTHREADS_ROOT}
-    $ENV{OSG_INC}
-    $ENV{OSG_ROOT}/lib64
-    $ENV{OSG_ROOT}/lib
-    $ENV{OSG_ROOT}
-)
-    mark_as_advanced (${MYLIBRARY})
-endmacro(FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
+    FIND_LIBRARY (${MYLIBRARY}
+        NAMES ${MYLIBRARYNAME}
+        HINTS
+        $ENV{TR_ROOT}/Ext/lib64
+        $ENV{TR_ROOT}/Ext/lib
+        $ENV{TR_ROOT}/Ext
+        $ENV{TR_LIB}
+        $ENV{TR_ROOT}/lib64
+        $ENV{TR_ROOT}/lib
+        $ENV{TR_ROOT}
+        $ENV{OPENTHREADS_LIB}
+        $ENV{OPENTHREADS_ROOT}/lib64
+        $ENV{OPENTHREADS_ROOT}/lib
+        $ENV{OPENTHREADS_ROOT}
+        $ENV{OSG_INC}
+        $ENV{OSG_ROOT}/lib64
+        $ENV{OSG_ROOT}/lib
+        $ENV{OSG_ROOT}
+        PATHS
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        ~/Library/Frameworks
+        /Library/Frameworks
+    )
+    MARK_AS_ADVANCED (${MYLIBRARY})
+ENDMACRO(FIND_OPENTHREADS_LIBRARY MYLIBRARY MYLIBRARYNAME)
 
-if (TR_BUILD_WITH_RELEASE)
+IF (TR_BUILD_WITH_RELEASE)
     # Locates all OPENTHREADS release libraries
     FIND_OPENTHREADS_LIBRARY (OPENTHREADS_LIBRARY OpenThreads)
-endif (TR_BUILD_WITH_RELEASE)
+ENDIF (TR_BUILD_WITH_RELEASE)
 
-if (TR_BUILD_WITH_DEBUG)
+IF (TR_BUILD_WITH_DEBUG)
     # Locates all OPENTHREADS debug libraries
     FIND_OPENTHREADS_LIBRARY (OPENTHREADS_LIBRARY_DEBUG OpenThreadsd)
-endif (TR_BUILD_WITH_DEBUG)
+ENDIF (TR_BUILD_WITH_DEBUG)
 
-set (DEPENDENCY_FOUND "NO")
-if (OPENTHREADS_LIBRARY AND OPENTHREADS_INCLUDE_DIR)
-    set (DEPENDENCY_FOUND "YES")
-endif (OPENTHREADS_LIBRARY AND OPENTHREADS_INCLUDE_DIR)
+SET (DEPENDENCY_FOUND "NO")
+IF (OPENTHREADS_LIBRARY AND OPENTHREADS_INCLUDE_DIR)
+    SET (DEPENDENCY_FOUND "YES")
+ENDIF (OPENTHREADS_LIBRARY AND OPENTHREADS_INCLUDE_DIR)
