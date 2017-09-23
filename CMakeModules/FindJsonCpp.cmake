daf55185314089d67276e8a503ebe5bf3a2abee3
DieSlower <maximster@gmail.com>
2017-09-23 14:38:34 -0400

    Issue #25 Adds cmake JSON macros.

---
+++ CMakeModules/FindJsonCpp.cmake
@@ -0,0 +1,60 @@
+# Finds the include files necessary for compilation
+FIND_PATH (JSON_CPP_INCLUDE_DIR json/json.h    
+    HINTS
+    $ENV{TR_INC}
+    $ENV{TR_ROOT}/Ext/include
+    $ENV{TR_ROOT}/Ext
+    $ENV{TR_ROOT}/include
+    $ENV{TR_ROOT}
+    $ENV{JSON_INC}
+    $ENV{JSON_ROOT}/include
+    $ENV{JSON_ROOT}
+    PATHS
+    /usr/include
+    /usr/local/include
+    ~/Library/Frameworks
+    /Library/Frameworks
+)
+MARK_AS_ADVANCED (JSON_CPP_INCLUDE_DIR)
+
+MACRO (FIND_JSON_CPP_LIBRARY MYLIBRARY MYLIBRARYNAME)
+    # Finds the library files necessary for compilation
+    FIND_LIBRARY (${MYLIBRARY}
+        NAMES ${MYLIBRARYNAME}
+        HINTS
+        $ENV{TR_ROOT}/Ext/lib64
+        $ENV{TR_ROOT}/Ext/lib
+        $ENV{TR_ROOT}/Ext
+        $ENV{TR_LIB}
+        $ENV{TR_ROOT}/lib64
+        $ENV{TR_ROOT}/lib
+        $ENV{TR_ROOT}
+        $ENV{JSON_LIB}
+        $ENV{JSON_ROOT}/lib64
+        $ENV{JSON_ROOT}/lib
+        $ENV{JSON_ROOT}
+        PATHS
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        ~/Library/Frameworks
+        /Library/Frameworks
+    )
+    MARK_AS_ADVANCED (${MYLIBRARY})
+ENDMACRO(FIND_JSON_CPP_LIBRARY MYLIBRARY MYLIBRARYNAME)
+
+IF (TC_BUILD_WITH_RELEASE)
+    # Locates all JSON_CPP release libraries
+    FIND_JSON_CPP_LIBRARY (JSON_CPP_LIBRARY jsoncpp)
+ENDIF (TC_BUILD_WITH_RELEASE)
+
+IF (TC_BUILD_WITH_DEBUG)
+    # Locates all JSON_CPP debug libraries
+    FIND_JSON_CPP_LIBRARY (JSON_CPP_LIBRARY_DEBUG jsoncppd)
+ENDIF (TC_BUILD_WITH_DEBUG)
+
+SET (DEPENDENCY_FOUND "NO")
+IF (JSON_CPP_INCLUDE_DIR)
+    SET (DEPENDENCY_FOUND "YES")
+ENDIF (JSON_CPP_INCLUDE_DIR)
