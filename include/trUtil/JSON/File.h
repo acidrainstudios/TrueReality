cb6697224fdb2ae146ae394064cfad950b578bd3
DieSlower <maximster@gmail.com>
2017-09-23 17:47:10 -0400

    Issue #55 Adds override notation to File methods.

--- include/trUtil/JSON/File.h
+++ include/trUtil/JSON/File.h
@@ -111,192 +111,192 @@ namespace trUtil
 			/**
 			* Returns a reference to the internal JSON Root node.
 			*/
-			virtual Json::Value& GetJSONRoot();
+			virtual Json::Value& GetJSONRoot() override;
 
 			/**
 			* Prints out to the screen the whole JSON Root content.
 			*/
-			virtual void PrintJSONRoot();
+			virtual void PrintJSONRoot() override;
 
 			/**
 			* Checks if the JSON Root Node has an entry with a given key present
 			*/
-			virtual bool KeyPresent(const std::string &key) const;
+			virtual bool KeyPresent(const std::string &key) const override;
 
 			/**
 			* Checks if the value stored at the specific key is a NULL
 			*/
-			virtual bool IsNull(const std::string &key) const;
+			virtual bool IsNull(const std::string &key) const override;
 
 			/**
 			* Sets the value stored at the specific key to NULL
 			*/
-			virtual void SetNull(const std::string &key);
+			virtual void SetNull(const std::string &key) override;
 
 			/**
 			* Checks if the value stored at the specific key is a Boolean
 			*/
-			virtual bool IsBool(const std::string &key) const;
+			virtual bool IsBool(const std::string &key) const override;
 
 			/**
 			* Returns the Boolean value stored at the given key
 			*/
-			virtual bool GetBool(const std::string &key) const;
+			virtual bool GetBool(const std::string &key) const override;
 
 			/**
 			* Sets the Boolean value to be stored at the given key
 			*/
-			virtual void SetBool(const std::string &key, const bool &value);
+			virtual void SetBool(const std::string &key, const bool &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is True
 			*/
-			virtual bool IsTrue(const std::string &key) const;
+			virtual bool IsTrue(const std::string &key) const override;
 
 			/**
 			* Checks if the value stored at the specific key is False
 			*/
-			virtual bool IsFalse(const std::string &key) const;
+			virtual bool IsFalse(const std::string &key) const override;
 
 			/**
 			* Checks if the value stored at the specific key is a Number
 			*/
-			virtual bool IsNumber(const std::string &key) const;
+			virtual bool IsNumber(const std::string &key) const override;
 
 			/**
 			* Checks if the value stored at the specific key is an Integer
 			*/
-			virtual bool IsInt(const std::string &key) const;
+			virtual bool IsInt(const std::string &key) const override;
 
 			/**
 			* Returns the Integer value stored at the given key
 			*/
-			virtual int GetInt(const std::string &key) const;
+			virtual int GetInt(const std::string &key) const override;
 
 			/**
 			* Sets the Integer value to be stored at the given key
 			*/
-			virtual void SetInt(const std::string &key, const int &value);
+			virtual void SetInt(const std::string &key, const int &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is a Double
 			*/
-			virtual bool IsDouble(const std::string &key) const;
+			virtual bool IsDouble(const std::string &key) const override;
 
 			/**
 			* Returns the Double value stored at the given key
 			*/
-			virtual double GetDouble(const std::string &key) const;
+			virtual double GetDouble(const std::string &key) const override;
 
 			/**
 			* Sets the Double value to be stored at the given key
 			*/
-			virtual void SetDouble(const std::string &key, const double &value);
+			virtual void SetDouble(const std::string &key, const double &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is an Unsigned Integer
 			*/
-			virtual bool IsUInt(const std::string &key) const;
+			virtual bool IsUInt(const std::string &key) const override;
 
 			/**
 			* Returns the Unsigned Integer value stored at the given key
 			*/
-			virtual unsigned int GetUInt(const std::string &key) const;
+			virtual unsigned int GetUInt(const std::string &key) const override;
 
 			/**
 			* Sets the Unsigned Integer value to be stored at the given key
 			*/
-			virtual void SetUInt(const std::string &key, const unsigned int &value);
+			virtual void SetUInt(const std::string &key, const unsigned int &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is a 64bit Integer
 			*/
-			virtual bool IsInt64(const std::string &key) const;
+			virtual bool IsInt64(const std::string &key) const override;
 
 			/**
 			* Returns the 64bit Integer value stored at the given key
 			*/
-			virtual Json::Value::Int64 GetInt64(const std::string &key) const;
+			virtual Json::Value::Int64 GetInt64(const std::string &key) const override;
 
 			/**
 			* Sets the 64bit Integer value to be stored at the given key
 			*/
-			virtual void SetInt64(const std::string &key, const Json::Value::Int64 &value);
+			virtual void SetInt64(const std::string &key, const Json::Value::Int64 &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is a 64bit Integer
 			*/
-			virtual bool IsUInt64(const std::string &key) const;
+			virtual bool IsUInt64(const std::string &key) const override;
 
 			/**
 			* Returns the 64bit Integer value stored at the given key
 			*/
-			virtual Json::Value::UInt64 GetUInt64(const std::string &key) const;
+			virtual Json::Value::UInt64 GetUInt64(const std::string &key) const override;
 
 			/**
 			* Sets the 64bit Integer value to be stored at the given key
 			*/
-			virtual void SetUInt64(const std::string &key, const Json::Value::UInt64 &value);
+			virtual void SetUInt64(const std::string &key, const Json::Value::UInt64 &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is a float
 			*/
-			virtual bool IsFloat(const std::string &key) const;
+			virtual bool IsFloat(const std::string &key) const override;
 
 			/**
 			* Returns the float value stored at the given key
 			*/
-			virtual float GetFloat(const std::string &key) const;
+			virtual float GetFloat(const std::string &key) const override;
 
 			/**
 			* Sets the float value to be stored at the given key
 			*/
-			virtual void SetFloat(const std::string &key, const float &value);
+			virtual void SetFloat(const std::string &key, const float &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is a String
 			*/
-			virtual bool IsString(const std::string &key) const;
+			virtual bool IsString(const std::string &key) const override;
 
 			/**
 			* Returns the String value stored at the given key
 			*/
-			virtual const std::string GetString(const std::string &key) const;
+			virtual const std::string GetString(const std::string &key) const override;
 
 			/**
 			* Sets the String to be stored at the given key
 			*/
-			virtual void SetString(const std::string &key, const std::string &value);
+			virtual void SetString(const std::string &key, const std::string &value) override;
 
 			/**
 			* Checks if the value stored at the specific key is an Array
 			*/
-			virtual bool IsArray(const std::string &key) const;
+			virtual bool IsArray(const std::string &key) const override;
 
 			/**
 			* Returns the Array value stored at the given key
 			*/
-			virtual Array GetArray(const std::string &key) const;
+			virtual Array GetArray(const std::string &key) const override;
 
 			/**
 			* Sets the Array to be stored at the given key
 			*/
-			virtual void SetArray(const std::string &key, Array &Array);
+			virtual void SetArray(const std::string &key, Array &Array) override;
 
 			/**
 			* Checks if the value stored at the specific key is an Object
 			*/
-			virtual bool IsObject(const std::string &key) const;
+			virtual bool IsObject(const std::string &key) const override;
 
 			/**
 			* Returns the Object value stored at the given key
 			*/
-			virtual Object GetObject(const std::string &key) const;
+			virtual Object GetObject(const std::string &key) const override;
 
 			/**
 			* Sets the Array to be stored at the given key
 			*/
-			virtual void SetObject(const std::string &key, Object &Object);
+			virtual void SetObject(const std::string &key, Object &Object) override;
 
 		protected:
 
