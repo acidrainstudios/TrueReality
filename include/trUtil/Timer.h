dcdb5701813ce4196b64326c1fec12de5f3f693f
DieSlower <maximster@gmail.com>
2017-10-05 00:07:40 -0400

    Issue #59 Adds a Timer class to the trUtil Library.

---
+++ include/trUtil/Timer.h
@@ -0,0 +1,203 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2017 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+#pragma once
+
+#include <trUtil/Export.h>
+
+#include <osg/Timer>
+
+namespace trUtil
+{
+    /**
+     * @typedef osg::Timer_t TimeTicks
+     *
+     * @brief   64bit Integer to hold the Timers time ticks.
+     */
+#if defined(_MSC_VER)
+    typedef __int64 TimeTicks;
+#else
+    typedef unsigned long long TimeTicks;
+#endif
+
+    /**
+     * @fn  void TR_UTIL_EXPORT AppSleep(unsigned int milliseconds);
+     *
+     * @brief   Puts the current application thread to sleep for a given number of milliseconds. 
+     *
+     * @param   milliseconds    The milliseconds.
+     *
+     * @return  A TR_UTIL_EXPORT.
+     */
+    void TR_UTIL_EXPORT AppSleep(unsigned int milliseconds);
+
+   /**
+    * Timer class is used for measuring elapsed time or time between two points.
+    */
+    class TR_UTIL_EXPORT Timer
+    {
+    public:
+        Timer();
+        ~Timer();
+
+        /**
+        * Creates and returns a global timer. 
+        * This allows the user to use the timer class as a singleton. 
+        * It can still be declared locally though, without the use of Instance()
+        */
+        static const Timer* Instance();
+
+        /**
+         * @fn  TimeTicks Timer::Tick() const
+         *
+         * @brief   Get the timers tick value.
+         *
+         * @return  The TimeTicks.
+         */
+        TimeTicks Tick() 
+        {
+            mOldTicks = mNewTicks;
+            mNewTicks = (mTimer.tick() + mCustomTicks);
+            return mNewTicks;
+        }
+
+        /**
+         * @fn  void Timer::SetStartTick()
+         *
+         * @brief   Set the start tick.
+         */
+        void SetStartTick() { mTimer.setStartTick(); }
+
+        /**
+         * @fn  void Timer::SetStartTick(TimeTicks t)
+         *
+         * @brief   Set the start tick to a custom value.
+         *
+         * @param   t   The TimeTicks to process.
+         */
+        void SetStartTick(TimeTicks t);
+
+        /**
+         * @fn  TimeTicks Timer::GetStartTick() const
+         *
+         * @brief   Get the value of the start tick.
+         *
+         * @return  The start tick.
+         */
+        TimeTicks GetStartTick() const { return mTimer.getStartTick(); }
+
+        /**
+         * @fn  double Timer::DeltaSec(TimeTicks t1, TimeTicks t2) const
+         *
+         * @brief   Get the time in seconds between timer ticks t1 and t2.
+         *
+         * @param   t1  The first TimeTicks.
+         * @param   t2  The second TimeTicks.
+         *
+         * @return  A double.
+         */
+        double DeltaSec(TimeTicks t1, TimeTicks t2) const { return mTimer.delta_s(t1, t2); }
+
+        /**
+         * @fn  double Timer::DeltaMil(TimeTicks t1, TimeTicks t2) const
+         *
+         * @brief   Get the time in milliseconds between timer ticks t1 and t2.
+         *
+         * @param   t1  The first TimeTicks.
+         * @param   t2  The second TimeTicks.
+         *
+         * @return  A double.
+         */
+        double DeltaMil(TimeTicks t1, TimeTicks t2) const { return mTimer.delta_m(t1, t2); }
+
+        /**
+         * @fn  double Timer::DeltaMicro(TimeTicks t1, TimeTicks t2) const
+         *
+         * @brief   Get the time in microseconds between timer ticks t1 and t2.
+         *
+         * @param   t1  The first TimeTicks.
+         * @param   t2  The second TimeTicks.
+         *
+         * @return  A double.
+         */
+        double DeltaMicro(TimeTicks t1, TimeTicks t2) const { return mTimer.delta_u(t1, t2); }
+
+        /**
+         * @fn  double Timer::DeltaNano(TimeTicks t1, TimeTicks t2) const
+         *
+         * @brief   Get the time in nanoseconds between timer ticks t1 and t2.
+         *
+         * @param   t1  The first TimeTicks.
+         * @param   t2  The second TimeTicks.
+         *
+         * @return  A double.
+         */
+        double DeltaNano(TimeTicks t1, TimeTicks t2) const { return mTimer.delta_n(t1, t2); }
+
+        /**
+         * @fn  double Timer::ElapsedSeconds() const
+         *
+         * @brief   Get elapsed time in seconds.
+         *
+         * @return  A double.
+         */
+        double ElapsedSeconds() const { return mTimer.time_s(); }
+
+        /**
+         * @fn  double Timer::ElapsedMilliseconds() const
+         *
+         * @brief   Get elapsed time in milliseconds.
+         *
+         * @return  A double.
+         */
+        double ElapsedMilliseconds() const { return mTimer.time_m(); }
+
+        /**
+         * @fn  double Timer::ElapsedMicroseconds() const
+         *
+         * @brief   Get elapsed time in microseconds.
+         *
+         * @return  A double.
+         */
+        double ElapsedMicroseconds() const { return mTimer.time_u(); }
+
+        /**
+        * Get elapsed time in nanoseconds.
+        */
+        double ElapsedNanoseconds() const { return mTimer.time_n(); }
+
+        /**
+        * Get the number of seconds per CPU tick.
+        */
+        double GetSecondsPerCPUTick() const { return mTimer.getSecondsPerTick(); }
+
+        /**
+        * Get the number of seconds per tick.
+        */
+        double GetSecondsPerTick() const { return (double)(mNewTicks - mOldTicks)*GetSecondsPerCPUTick(); }
+
+    private:
+        osg::Timer mTimer;
+        TimeTicks mOldTicks = 0;
+        TimeTicks mNewTicks = 0;
+        TimeTicks mCustomTicks = 0;
+    };
+}
+
