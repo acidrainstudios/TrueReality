c7d7fc3651014536600e48a5416bed6639d41fda
DieSlower <maximster@gmail.com>
2017-10-07 01:03:44 -0400

    Issue #59 Adds the export macros for trBase.

---
+++ include/trBase/Export.h
@@ -0,0 +1,36 @@
+/*
+* The Construct Open Source Game and Simulation Engine
+* Copyright (C) 2017
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+
+#pragma once
+
+#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
+#  ifdef TR_BASE_LIBRARY
+#    define TR_BASE_EXPORT __declspec(dllexport)
+#  else
+#    define TR_BASE_EXPORT __declspec(dllimport)
+#  endif
+#else
+#  ifdef TR_BASE_LIBRARY
+#    define TR_BASE_EXPORT  __attribute__ ((visibility("default")))
+#  else
+#    define TR_BASE_EXPORT
+#  endif
+#endif
