c7b8cd5eb951509ac30b93467384c2a40ab6bfcf
DieSlower <maximster@gmail.com>
2017-10-07 01:46:55 -0400

    Issue #25 Adds cmake FindBoost macros.

---
+++ CMakeModules/FindBoostLibs.cmake
@@ -0,0 +1,43 @@
+# *****************************************************************************
+# *****************************************************************************
+# FindBoostLibs uses CMakes FindBoost to find specific boost libraries ********
+# *****************************************************************************
+# All search options and Boost configurations should be placed here ***********
+# *****************************************************************************
+# *****************************************************************************
+FIND_PACKAGE (Boost 1.61.0 REQUIRED COMPONENTS 
+    #atomic 
+    chrono 
+    #container 
+    #context 
+    #coroutine 
+    date_time         
+    #filesystem 
+    #graph 
+    #iostreams 
+    #locale 
+    #log_setup 
+    #log        
+    #math_c99f 
+    #math_c99l 
+    #math_c99 
+    #math_tr1f 
+    #math_tr1l 
+    #math_tr1         
+    #prg_exec_monitor 
+    program_options 
+    #random 
+    #regex 
+    #serialization 
+    #signals 
+    system 
+    thread 
+    #timer 
+    #type_erasure 
+    unit_test_framework 
+    #wave 
+    #wserialization        
+    )
+# *****************************************************************************
+# *****************************************************************************
+# *****************************************************************************
