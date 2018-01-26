854f0a8dcf5e42d942f3efdc1d0928ca21418983
DieSlower <maximster@gmail.com>
2018-01-25 20:00:13 -0500

    Issue #9 Adds an exception for invalid function parameters

---
+++ src/trUtil/ExceptionInvalidParameter.cpp
@@ -0,0 +1,31 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2018 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+
+#include <trUtil/ExceptionInvalidParameter.cpp.h>
+
+namespace trUtil
+{
+    ////////////////////////////////////////////////////////////////////////////////
+    trUtil::ExceptionInvalidParameter::ExceptionInvalidParameter(const std::string& message, const std::string& filename, unsigned int linenum)
+        : trUtil::Exception(message, filename, linenum)
+    {
+    }
+}
