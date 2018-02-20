3f96b1c872413789b8dbb3eb182ae8377c2965c2
DieSlower <maximster@gmail.com>
2018-02-19 22:40:10 -0500

    Issue #59 Adds a Director Example

---
+++ Examples/Director/Application.h
@@ -0,0 +1,60 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2018 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+
+#pragma once
+
+#include <trApp/AppBase.h>
+
+class Application : public trApp::AppBase
+{
+public:
+    typedef trApp::AppBase BaseClass;                 /// Adds an easy and swappable access to the base class
+
+    const static trUtil::RefStr CLASS_TYPE;         /// Holds the class type name for efficient comparisons
+
+    /**
+     * @fn  Application::Application(const std::string name = CLASS_TYPE);
+     *
+     * @brief   Constructor.
+     *
+     * @param   name    (Optional) The name.
+     */
+    Application(const std::string name = CLASS_TYPE);
+
+    /**
+     * @fn  virtual const std::string& Application::GetType() const override;
+     *
+     * @brief   Gets the type.
+     *
+     * @return  The type.
+     */
+    virtual const std::string& GetType() const override;
+
+protected:
+
+    /**
+     * @fn  Application::~Application();
+     *
+     * @brief   Destructor.
+     */
+    ~Application();
+};
+
---
+++ Examples/Director/TestDirector2.h
@@ -0,0 +1,108 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2018 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Maxim Serebrennik
+*/
+#pragma once
+
+#include <trManager/DirectorBase.h>
+#include <trUtil/RefStr.h>
+
+#include <string>
+
+class TestDirector2 : public trManager::DirectorBase
+{
+public:
+    typedef trManager::DirectorBase BaseClass;      /// Adds an easy and swappable access to the base class
+
+    const static trUtil::RefStr CLASS_TYPE;         /// Holds the class type name for efficient comparisons
+
+    /**
+     * @fn  TestDirector2::TestDirector2(const std::string name = CLASS_TYPE);
+     *
+     * @brief   Constructor.
+     *
+     * @param   name    (Optional) The name.
+     */
+    TestDirector2(const std::string name = CLASS_TYPE);
+
+    /**
+     * @fn  virtual const std::string& TestDirector2::GetType() const override
+     *
+     * @brief   Gets the class type.
+     *
+     * @return  The type.
+     */
+    virtual const std::string& GetType() const override { return CLASS_TYPE;}
+
+    /**
+     * @fn  virtual void TestDirector2::OnMessage(const trManager::MessageBase& msg);
+     *
+     * @brief   This function receives messages coming from the System Manager.
+     *
+     * @param   msg The message.
+     */
+    virtual void OnMessage(const trManager::MessageBase& msg);
+
+    /**
+     * @fn  virtual void TestDirector2::OnTick(const trManager::MessageBase& msg);
+     *
+     * @brief   Executes on Reception of the Tick Message.
+     *
+     * @param   msg The message.
+     */
+    virtual void OnTick(const trManager::MessageBase& msg);
+
+    /**
+     * @fn  virtual void TestDirector2::OnAddedToSysMan() override;
+     *
+     * @brief   Runs when the director is added to system manager.
+     */
+    virtual void OnAddedToSysMan() override;
+
+    /**
+     * @fn  static int TestDirector2::GetInstCount();
+     *
+     * @brief   Gets instance count for this class type. Used for error checking, and Unit Testing.
+     *
+     * @return  The instance count.
+     */
+    static int GetInstCount();
+
+protected:
+
+    static int mInstCount;
+
+    /**
+     * @fn  TestDirector2::~TestDirector2();
+     *
+     * @brief   Destructor.
+     */
+    ~TestDirector2();
+
+    /**
+     * @fn  virtual void TestDirector2::HandleTickMessage(const trManager::MessageBase& msg);
+     *
+     * @brief   Handles the tick message.
+     *
+     * @param   msg The message.
+     */
+    virtual void HandleTickMessage(const trManager::MessageBase& msg);
+
+};
+
