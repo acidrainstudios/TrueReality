e7f6d732857c196ead5bedb6179765c6658bb77b
DieSlower <maximster@gmail.com>
2018-07-16 09:24:39 -0400

    Issue #62 Updates AppBase class to use using instead of typedef to be c++11 complient

--- include/trApp/AppBase.h
+++ include/trApp/AppBase.h
@@ -38,7 +38,7 @@ namespace trApp
     class TR_APP_EXPORT AppBase : public trBase::Base
     {
     public:
-        typedef trBase::Base BaseClass;                 /// Adds an easy and swappable access to the base class
+        using BaseClass = trBase::Base;                 /// Adds an easy and swappable access to the base class
 
         const static trUtil::RefStr CLASS_TYPE;         /// Holds the class type name for efficient comparisons
 
