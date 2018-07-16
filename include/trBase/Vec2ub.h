1e895b2a0addb37004c062c69cb4ece6c8616055
DieSlower <maximster@gmail.com>
2018-07-16 10:03:49 -0400

    Issue #62 Updates Vec2ub class to use using instead of typedef to be c++11 complient

--- include/trBase/Vec2ub.h
+++ include/trBase/Vec2ub.h
@@ -41,7 +41,7 @@ namespace trBase
     public:
 
         /** Data type of vector components.*/
-        typedef unsigned char value_type;
+        using value_type = unsigned char;
 
         const static int NUM_OF_COMPONENTS;
 
