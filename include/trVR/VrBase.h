7f15362cb9042c7328ea0376b477a04e3499e90c
Alexander Koufos <exoticdft@gmail.com>
2018-07-24 19:41:23 -0400

    Issue #18 Adds initial files to represent trVR library. 	- Adds the include folder trVR with the Export and VrBase headers (no functionality.) 	- Adds the source folder trVR with VrBase cpp file (no functionality) and CMakeLists.txt file used for adding trVR to the project.

---
+++ include/trVR/VrBase.h
@@ -0,0 +1,40 @@
+/*
+ * True Reality Open Source Game and Simulation Engine
+ * Copyright � 2018 Acid Rain Studios LLC
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Alexander Koufos <alexander.p.koufos@leidos.com>
+ *
+ * Created on July 24, 2018, 7:08 PM
+ */
+#pragma once
+
+#include <trVR/Export.h>
+
+namespace trVR
+{
+    class TR_VR_EXPORT VrBase
+    {
+    public:
+        VrBase() = default;
+
+        VrBase(const VrBase& orig) = delete;
+        VrBase& operator=(const VrBase& orig) = delete;
+    protected:
+        virtual ~VrBase() = default;
+    private:
+    };
+}
