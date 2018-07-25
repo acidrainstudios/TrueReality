dad4722b0806e56e71af893e518bad8f22c8251c
DieSlower <maximster@gmail.com>
2018-07-25 12:19:52 -0400

    Issue #25 Updates FindOSG CMake scripts

--- CMakeModules/FindOSG.cmake
+++ CMakeModules/FindOSG.cmake
@@ -73,14 +73,14 @@ SET(DEPENDENCY "OSG")
 # Finds the include files necessary for compilation
 FIND_PATH (${DEPENDENCY}_INCLUDE_DIR osg/Referenced
     HINTS
+    $ENV{OSG_INC}
+    $ENV{OSG_ROOT}/include
+    $ENV{OSG_ROOT}
     $ENV{TR_INC}
     $ENV{TR_ROOT}/Ext/include
     $ENV{TR_ROOT}/Ext
     $ENV{TR_ROOT}/include
     $ENV{TR_ROOT}
-    $ENV{OSG_INC}
-    $ENV{OSG_ROOT}/include
-    $ENV{OSG_ROOT}
     PATHS
     /usr/include
     /usr/local/include
@@ -94,16 +94,16 @@ MACRO (FIND_DEPENDENCY_LIBRARY MYLIBRARY MYLIBRARYNAME)
     FIND_LIBRARY (${MYLIBRARY}
 		NAMES ${MYLIBRARYNAME}
 		HINTS
-		$ENV{TR_ROOT}/Ext/lib64
-		$ENV{TR_ROOT}/Ext/lib
-		$ENV{TR_ROOT}/Ext
-		$ENV{TR_LIB}
-		$ENV{TR_ROOT}/lib
-		$ENV{TR_ROOT}
 		$ENV{OSG_LIB}
 		$ENV{OSG_ROOT}/lib64
 		$ENV{OSG_ROOT}/lib
 		$ENV{OSG_ROOT}
+		$ENV{TR_LIB}
+		$ENV{TR_ROOT}/Ext/lib64
+		$ENV{TR_ROOT}/Ext/lib
+		$ENV{TR_ROOT}/Ext
+		$ENV{TR_ROOT}/lib
+		$ENV{TR_ROOT}
 		PATHS
 		/usr/lib64
 		/usr/lib
@@ -113,55 +113,55 @@ MACRO (FIND_DEPENDENCY_LIBRARY MYLIBRARY MYLIBRARYNAME)
 		/Library/Frameworks
     )
     MARK_AS_ADVANCED (${MYLIBRARY})
-ENDMACRO(FIND_DEPENDENCY_LIBRARY MYLIBRARY MYLIBRARYNAME)
+ENDMACRO()
 
 # Try to ascertain the version...
-IF(${DEPENDENCY}_INCLUDE_DIR)
-    IF(OpenSceneGraph_DEBUG)
+IF (${DEPENDENCY}_INCLUDE_DIR)
+    IF (OpenSceneGraph_DEBUG)
         MESSAGE(STATUS "[ FindOpenSceneGraph.cmake:${CMAKE_CURRENT_LIST_LINE} ] "
             "Detected OSG_INCLUDE_DIR = ${OSG_INCLUDE_DIR}")
-    ENDIF()
+    ENDIF ()
 
-    SET(_osg_Version_file "${OSG_INCLUDE_DIR}/osg/Version")
-    IF("${OSG_INCLUDE_DIR}" MATCHES "\\.framework$" AND NOT EXISTS "${_osg_Version_file}")
-        SET(_osg_Version_file "${OSG_INCLUDE_DIR}/Headers/Version")
-    ENDIF()
+    SET (_osg_Version_file "${OSG_INCLUDE_DIR}/osg/Version")
+    IF ("${OSG_INCLUDE_DIR}" MATCHES "\\.framework$" AND NOT EXISTS "${_osg_Version_file}")
+        SET (_osg_Version_file "${OSG_INCLUDE_DIR}/Headers/Version")
+    ENDIF ()
 
-    IF(EXISTS "${_osg_Version_file}")
-      FILE(STRINGS "${_osg_Version_file}" _osg_Version_contents
+    IF (EXISTS "${_osg_Version_file}")
+      FILE (STRINGS "${_osg_Version_file}" _osg_Version_contents
            REGEX "#define (OSG_VERSION_[A-Z]+|OPENSCENEGRAPH_[A-Z]+_VERSION)[ \t]+[0-9]+")
-    ELSE()
-      SET(_osg_Version_contents "unknown")
-    ENDIF()
+    ELSE ()
+      SET (_osg_Version_contents "unknown")
+    ENDIF ()
 
-    STRING(REGEX MATCH ".*#define OSG_VERSION_MAJOR[ \t]+[0-9]+.*"
+    STRING (REGEX MATCH ".*#define OSG_VERSION_MAJOR[ \t]+[0-9]+.*"
         _osg_old_defines "${_osg_Version_contents}")
-    STRING(REGEX MATCH ".*#define OPENSCENEGRAPH_MAJOR_VERSION[ \t]+[0-9]+.*"
+    STRING (REGEX MATCH ".*#define OPENSCENEGRAPH_MAJOR_VERSION[ \t]+[0-9]+.*"
         _osg_new_defines "${_osg_Version_contents}")
-    IF(_osg_old_defines)
-        STRING(REGEX REPLACE ".*#define OSG_VERSION_MAJOR[ \t]+([0-9]+).*"
+    IF (_osg_old_defines)
+        STRING (REGEX REPLACE ".*#define OSG_VERSION_MAJOR[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_MAJOR ${_osg_Version_contents})
-        STRING(REGEX REPLACE ".*#define OSG_VERSION_MINOR[ \t]+([0-9]+).*"
+        STRING (REGEX REPLACE ".*#define OSG_VERSION_MINOR[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_MINOR ${_osg_Version_contents})
-        STRING(REGEX REPLACE ".*#define OSG_VERSION_PATCH[ \t]+([0-9]+).*"
+        STRING (REGEX REPLACE ".*#define OSG_VERSION_PATCH[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_PATCH ${_osg_Version_contents})
-    ELSEIF(_osg_new_defines)
-        STRING(REGEX REPLACE ".*#define OPENSCENEGRAPH_MAJOR_VERSION[ \t]+([0-9]+).*"
+    ELSEIF (_osg_new_defines)
+        STRING (REGEX REPLACE ".*#define OPENSCENEGRAPH_MAJOR_VERSION[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_MAJOR ${_osg_Version_contents})
-        STRING(REGEX REPLACE ".*#define OPENSCENEGRAPH_MINOR_VERSION[ \t]+([0-9]+).*"
+        STRING (REGEX REPLACE ".*#define OPENSCENEGRAPH_MINOR_VERSION[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_MINOR ${_osg_Version_contents})
-        STRING(REGEX REPLACE ".*#define OPENSCENEGRAPH_PATCH_VERSION[ \t]+([0-9]+).*"
+        STRING (REGEX REPLACE ".*#define OPENSCENEGRAPH_PATCH_VERSION[ \t]+([0-9]+).*"
             "\\1" _osg_VERSION_PATCH ${_osg_Version_contents})
-    ELSE()
-        MESSAGE(WARNING "[ FindOpenSceneGraph.cmake:${CMAKE_CURRENT_LIST_LINE} ] "
+    ELSE ()
+        MESSAGE (WARNING "[ FindOpenSceneGraph.cmake:${CMAKE_CURRENT_LIST_LINE} ] "
             "Failed to parse version number, please report this as a bug")
-    ENDIF()
-    UNSET(_osg_Version_contents)
+    ENDIF ()
+    UNSET (_osg_Version_contents)
 
-    SET(OPENSCENEGRAPH_VERSION "${_osg_VERSION_MAJOR}.${_osg_VERSION_MINOR}.${_osg_VERSION_PATCH}"
+    SET (OPENSCENEGRAPH_VERSION "${_osg_VERSION_MAJOR}.${_osg_VERSION_MINOR}.${_osg_VERSION_PATCH}"
                                 CACHE INTERNAL "The version of OSG which was detected")
-    IF(OpenSceneGraph_DEBUG)
-        MESSAGE(STATUS "[ FindOpenSceneGraph.cmake:${CMAKE_CURRENT_LIST_LINE} ] "
+    IF (OpenSceneGraph_DEBUG)
+        MESSAGE (STATUS "[ FindOpenSceneGraph.cmake:${CMAKE_CURRENT_LIST_LINE} ] "
             "Detected version ${OPENSCENEGRAPH_VERSION}")
     ENDIF()
 ENDIF()
@@ -187,7 +187,7 @@ IF (TR_BUILD_WITH_RELEASE)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_VIEWER_LIBRARY osgViewer)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_VOLUME_LIBRARY osgVolume)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_WIDGET_LIBRARY osgWidget)
-ENDIF (TR_BUILD_WITH_RELEASE)
+ENDIF ()
 
 IF (TR_BUILD_WITH_DEBUG)
     # Locates all debug libraries
@@ -210,16 +210,15 @@ IF (TR_BUILD_WITH_DEBUG)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_VIEWER_LIBRARY_DEBUG osgViewerd)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_VOLUME_LIBRARY_DEBUG osgVolumed)
     FIND_DEPENDENCY_LIBRARY (${DEPENDENCY}_WIDGET_LIBRARY_DEBUG osgWidgetd)
-ENDIF (TR_BUILD_WITH_DEBUG)
+ENDIF ()
 
 SET (${DEPENDENCY}_FOUND "NO")
 IF (${DEPENDENCY}_LIBRARY AND ${DEPENDENCY}_INCLUDE_DIR)
     SET (${DEPENDENCY}_FOUND "YES")
     
-    IF(${DEPENDENCY}_FIND_VERSION)
+    IF (${DEPENDENCY}_FIND_VERSION)
         CHECK_VERSION(${DEPENDENCY} ${OPENSCENEGRAPH_VERSION} ${OSG_FIND_VERSION})	
-    ELSE()
-        MESSAGE(STATUS "OSG Version: ${OPENSCENEGRAPH_VERSION}")
-    ENDIF()
-
-ENDIF (${DEPENDENCY}_LIBRARY AND ${DEPENDENCY}_INCLUDE_DIR)
+    ELSE ()
+        MESSAGE (STATUS "OSG Version: ${OPENSCENEGRAPH_VERSION}")
+    ENDIF ()
+ENDIF ()
