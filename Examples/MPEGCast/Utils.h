9f5557d1b2f3f1c87c9fd2b2e2409de541eafade
DieSlower <maximster@gmail.com>
2018-08-08 18:06:52 -0400

    Issue #69 Adds the initial outline files for the MPEG broadcasting example

---
+++ Examples/MPEGCast/Utils.h
@@ -0,0 +1,31 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2018 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* @author Maxim Serebrennik
+*/
+#pragma once
+
+#include <string>
+
+static const std::string PROGRAM_NAME = "TrueReality";
+static const std::string EXE_NAME = "exampleMPEGCast";
+
+/*
+* Parses the command line variables that are passed in to the executable
+*/
+void ParseCmdLineArgs(int& argc, char** argv);
