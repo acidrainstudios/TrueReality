29344fd325b36e11a7563b47aad31552ebf305a9
DieSlower <maximster@gmail.com>
2018-08-22 14:26:53 -0400

    Issue #69 Adds first project files for the MPEG consumption demo

---
+++ Examples/MPEGSlave/Main.cpp
@@ -0,0 +1,39 @@
+/*
+* True Reality Open Source Game and Simulation Engine
+* Copyright © 2018 Acid Rain Studios LLC
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 3.0 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* @author Maxim Serebrennik
+*/
+
+#pragma once
+
+#include <iostream>
+
+/**
+ * @fn  int main(int argc, char** argv)
+ *
+ * @brief   Main entry-point for this application.
+ *
+ * @param   argc    The number of command-line arguments provided.
+ * @param   argv    An array of command-line argument strings.
+ *
+ * @return  Exit-code for the process - 0 for success, else an error code.
+ */
+void main(int argc, char** argv)
+{
+    std::cout << "OSG Mpeg Slave Demo\n\n" << std::endl;  
+}
