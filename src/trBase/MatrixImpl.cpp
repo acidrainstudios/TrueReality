8cc6e39049b5041b7d8616fec68a0784d029604e
DieSlower <maximster@gmail.com>
2018-08-28 13:57:17 -0400

    Issue #62 Fixes constructor initialization for the Matrix classes class

--- src/trBase/MatrixImpl.cpp
+++ src/trBase/MatrixImpl.cpp
@@ -35,26 +35,26 @@ namespace trBase
 
     //////////////////////////////////////////////////////////////////////////
     MatrixImpl::MatrixImpl(const MatrixImpl& mat) 
+        : mMatrix(mat)
     { 
-        mMatrix.set(mat.GetOSGMatrix().ptr()); 
     }
 
     //////////////////////////////////////////////////////////////////////////
     MatrixImpl::MatrixImpl(float const * const ptr) 
+        : mMatrix(ptr)
     { 
-        mMatrix.set(ptr); 
     }
 
     //////////////////////////////////////////////////////////////////////////
     MatrixImpl::MatrixImpl(double const * const ptr) 
+        : mMatrix(ptr)
     { 
-        mMatrix.set(ptr); 
     }
 
     //////////////////////////////////////////////////////////////////////////
     MatrixImpl::MatrixImpl(const Quat& quat) 
+        : mMatrix(quat)
     { 
-        mMatrix.makeRotate(quat); 
     }
 
     //////////////////////////////////////////////////////////////////////////
@@ -62,9 +62,8 @@ namespace trBase
         value_type a10, value_type a11, value_type a12, value_type a13,
         value_type a20, value_type a21, value_type a22, value_type a23,
         value_type a30, value_type a31, value_type a32, value_type a33)
+        : mMatrix(a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33)
     {
-        mMatrix.set(a00, a01, a02, a03, a10, a11, a12, a13,
-            a20, a21, a22, a23, a30, a31, a32, a33);
     }
 
     //////////////////////////////////////////////////////////////////////////
