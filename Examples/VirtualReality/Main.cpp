cf5b996cd902a868715545aa4e80eea679e90227
Alexander Koufos <exoticdft@gmail.com>
2018-09-05 09:41:13 -0400

    Issue #78 Adds functionality to VR example
    - Replaces some window creation with that from the osgopenvrveiwerexample code.
    - Adds glew to try to create needed GL textures for OpenVR.

--- Examples/VirtualReality/Main.cpp
+++ Examples/VirtualReality/Main.cpp
@@ -18,6 +18,7 @@
 *
 * @author Alexander Koufos
 */
+#include <GL/glew.h>
 
 #include <Examples/VirtualReality/Utils.h>
 #include <Examples/VirtualReality/WinDefaultConfig.h>
@@ -60,20 +61,20 @@ static const double CAM_FOV = 25;
 static const int SAMPLE_NUM = 4;
 
 //////////////////////////////////////////////////////////////////////////
-osg::Texture2D* GenerateTexture(int screenWidth, int screenHeight, GLenum pxlFormat)
+osg::Texture2D* GenerateTexture(int renderWidth, int renderHeight, GLenum pxlFormat)
 {
     osg::Texture2D* textureTargetPtr = new osg::Texture2D();
     textureTargetPtr->setResizeNonPowerOfTwoHint(false);
-    textureTargetPtr->setTextureSize(screenWidth, screenHeight);
+    textureTargetPtr->setTextureSize(renderWidth, renderHeight);
     textureTargetPtr->setInternalFormat(pxlFormat);
     textureTargetPtr->setFilter(osg::Texture2D::MIN_FILTER, osg::Texture2D::LINEAR);
 //    textureTargetPtr->setFilter(osg::Texture2D::MAG_FILTER, osg::Texture2D::LINEAR);
 
-//    trBase::SmrtPtr<osg::Image> image = new osg::Image();
-//    image->allocateImage(WIN_WIDTH, WIN_HEIGHT, 1, GL_RGBA, GL_UNSIGNED_BYTE);
-    trBase::SmrtPtr<osg::Image> image = osgDB::readImageFile(trUtil::PathUtils::GetTexturesPath() + "/Grid/Grid.jpg");
-//    image->setPixelFormat(GL_RGBA);
-//    image->setDataType(GL_UNSIGNED_BYTE);
+    trBase::SmrtPtr<osg::Image> image = new osg::Image();
+    image->allocateImage(renderWidth, renderHeight, 1, GL_RGBA8, GL_UNSIGNED_BYTE);
+    //trBase::SmrtPtr<osg::Image> image = osgDB::readImageFile(trUtil::PathUtils::GetTexturesPath() + "/Grid/Grid.jpg");
+    //image->setPixelFormat(GL_RGBA);
+    //image->setDataType(GL_UNSIGNED_BYTE);
     textureTargetPtr->setImage(image.Get());
 
     return textureTargetPtr;
@@ -82,14 +83,14 @@ osg::Texture2D* GenerateTexture(int screenWidth, int screenHeight, GLenum pxlFor
 //////////////////////////////////////////////////////////////////////////
 osg::Geode* GenerateRenderTarget(osg::Texture2D* texture)
 {
-    int screenWidth = texture->getTextureWidth();
-    int screenHeight = texture->getTextureHeight();
+    int texWidth = texture->getTextureWidth();
+    int texHeight = texture->getTextureHeight();
 
     trBase::SmrtPtr<osg::Vec3Array> verticies = new osg::Vec3Array();
-    verticies->push_back(osg::Vec3(-screenWidth, 0.0, -screenHeight));
-    verticies->push_back(osg::Vec3(-screenWidth, 0.0, screenHeight));
-    verticies->push_back(osg::Vec3(screenWidth, 0.0, -screenHeight));
-    verticies->push_back(osg::Vec3(screenWidth, 0.0, screenHeight));
+    verticies->push_back(osg::Vec3(-texWidth, 0.0, -texHeight));
+    verticies->push_back(osg::Vec3(-texWidth, 0.0, texHeight));
+    verticies->push_back(osg::Vec3(texWidth, 0.0, -texHeight));
+    verticies->push_back(osg::Vec3(texWidth, 0.0, texHeight));
 
     trBase::SmrtPtr<osg::Vec2Array> texCoords = new osg::Vec2Array();
     texCoords->push_back(osg::Vec2(0.0, 0.0));
@@ -120,19 +121,67 @@ osg::Geode* GenerateRenderTarget(osg::Texture2D* texture)
 //////////////////////////////////////////////////////////////////////////
 GLuint FboTest(const osg::State& state, int width, int height)
 {
-//    const osg::GLExtensions* fbo_ext = state.get<osg::GLExtensions>();
-//    GLuint testFbo;
-//    GLuint testTex;
-//    
-//    fbo_ext->glGenFramebuffers(1, &testFbo);
+    const osg::GLExtensions* fbo_ext = state.get<osg::GLExtensions>();
+    GLuint testFbo;
+    GLuint testTex;
     
-//    glGenTextures(1, &testTex);
-//    glBindTexture(GL_TEXTURE_2D, testTex);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    fbo_ext->glGenFramebuffers(1, &testFbo);
     
-//    return testTex;
+    glGenTextures(1, &testTex);
+    glBindTexture(GL_TEXTURE_2D, testTex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    
+    return testTex;
+}
+
+//////////////////////////////////////////////////////////////////////////
+osg::GraphicsContext::Traits* CreateGraphicsContextTraits()
+{
+	osg::GraphicsContext::WindowingSystemInterface* wsi = osg::GraphicsContext::getWindowingSystemInterface();
+
+	if (!wsi)
+	{
+		osg::notify(osg::NOTICE) << "Error, no WindowSystemInterface available, cannot create windows." << std::endl;
+		return 0;
+	}
+
+	// Get the screen identifiers set in environment variable DISPLAY
+	osg::GraphicsContext::ScreenIdentifier si;
+	si.readDISPLAY();
+
+	// If displayNum has not been set, reset it to 0.
+	if (si.displayNum < 0)
+	{
+		si.displayNum = 0;
+		osg::notify(osg::INFO) << "Couldn't get display number, setting to 0" << std::endl;
+	}
+
+	// If screenNum has not been set, reset it to 0.
+	if (si.screenNum < 0)
+	{
+		si.screenNum = 0;
+		osg::notify(osg::INFO) << "Couldn't get screen number, setting to 0" << std::endl;
+	}
+
+	unsigned int width, height;
+	wsi->getScreenResolution(si, width, height);
+
+	osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+	traits->hostName = si.hostName;
+	traits->screenNum = si.screenNum;
+	traits->displayNum = si.displayNum;
+	traits->windowDecoration = true;
+	traits->x = 50;
+	traits->y = 50;
+	traits->width = 800;
+	traits->height = 450;
+	traits->doubleBuffer = true;
+	traits->sharedContext = nullptr;
+	traits->vsync = false; // VSync should always be disabled for because HMD submit handles the timing of the swap.
+
+	return traits.release();
 }
 
 /**
@@ -154,30 +203,76 @@ int main(int argc, char** argv)
         //Show Logo
         trUtil::Console::Logo();
 
-        trBase::SmrtPtr<osg::Texture2D> textureTarget = GenerateTexture(WIN_WIDTH, WIN_HEIGHT, GL_RGBA8);
+		//GLenum nGlewError = glewInit();
+		//if (nGlewError != GLEW_OK)
+		//{
+		//	printf("%s - Error initializing GLEW! %s\n", __FUNCTION__, glewGetErrorString(nGlewError));
+		//	return false;
+		//}
+		//glGetError(); // to clear the error caused deep in GLEW
+
+		trBase::SmrtPtr<trVR::VrBase> vrInstance = new trVR::VrBase;
+
+		vrInstance->Init();
+
+		uint32_t renderHeight, renderWidth;
+		vrInstance->GetVrSystem()->GetRecommendedRenderTargetSize(&renderWidth, &renderHeight);
+
+        trBase::SmrtPtr<osg::Texture2D> textureTarget = GenerateTexture(renderWidth, renderHeight, GL_RGBA8);
         trBase::SmrtPtr<osg::Geode> rootNode = GenerateRenderTarget(textureTarget);
 
         //Create our View
-        trBase::SmrtPtr<osgViewer::View> mainView = new osgViewer::View();
-        mainView->setSceneData(rootNode.Get());
-        mainView->apply(new WinDefaultConfig(WIN_POS_X, WIN_POS_Y, WIN_WIDTH, WIN_HEIGHT, 0U, true, CAM_NEAR_CLIP, CAM_FAR_CLIP, CAM_FOV));
+        //trBase::SmrtPtr<osgViewer::View> mainView = new osgViewer::View();
+        //mainView->setSceneData(rootNode.Get());
+        //mainView->apply(new WinDefaultConfig(WIN_POS_X, WIN_POS_Y, WIN_WIDTH, WIN_HEIGHT, 0U, true, CAM_NEAR_CLIP, CAM_FAR_CLIP, CAM_FOV));
 
-        //Adds the statistics handler. 
-        mainView->addEventHandler(new osgViewer::StatsHandler);
+		// Get the suggested context traits
+		osg::ref_ptr<osg::GraphicsContext::Traits> traits = CreateGraphicsContextTraits();
+		traits->windowName = "TR - Example VR";
 
-        //Set up the viewer
-        osgViewer::CompositeViewer viewer;
-        viewer.addView(mainView);
-        
-        trBase::SmrtPtr<trVR::VrBase> vrInstance = new trVR::VrBase;
-        
-        vrInstance->Init();
+		// Create a graphic context based on our desired traits
+		osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits);
 
-        //Set up the main frame loop
-        if ((mainView->getCameraManipulator() == 0) && mainView->getCamera()->getAllowEventFocus())
-        {
-            mainView->setCameraManipulator(new osgGA::TrackballManipulator());
-        }
+		if (!gc)
+		{
+			osg::notify(osg::NOTICE) << "Error, GraphicsWindow has not been created successfully" << std::endl;
+			return 1;
+		}
+
+		if (gc.valid())
+		{
+			gc->setClearColor(osg::Vec4(0.2f, 0.2f, 0.4f, 1.0f));
+			gc->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		}
+
+		// Create Trackball manipulator
+		osg::ref_ptr<osgGA::CameraManipulator> cameraManipulator = new osgGA::TrackballManipulator;
+
+		//Set up the viewer
+		osgViewer::Viewer viewer;
+
+		// Force single threaded to make sure that no other thread can use the GL context
+		viewer.setThreadingModel(osgViewer::Viewer::SingleThreaded);
+		viewer.getCamera()->setGraphicsContext(gc);
+		viewer.getCamera()->setViewport(0, 0, traits->width, traits->height);
+
+		// Disable automatic computation of near and far plane
+		viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR);
+		viewer.setCameraManipulator(cameraManipulator);
+
+        ////Adds the statistics handler. 
+        //mainView->addEventHandler(new osgViewer::StatsHandler);
+
+        ////Set up the viewer
+        //osgViewer::CompositeViewer viewer;
+        //viewer.addView(mainView);
+
+        ////Set up the main frame loop
+        //if ((mainView->getCameraManipulator() == 0) && mainView->getCamera()->getAllowEventFocus())
+        //{
+        //    mainView->setCameraManipulator(new osgGA::TrackballManipulator());
+        //}
+
         viewer.setReleaseContextAtEndOfFrameHint(true);
         
         if (!viewer.isRealized())
@@ -190,12 +285,14 @@ int main(int argc, char** argv)
         //while (vrInstance->IsSystemReady())
         //for (unsigned int i = 0; i < 1000; ++i)
         {
+			GLuint test = FboTest(*(gc->getState()), renderWidth, renderHeight);
             vrInstance->GetHeadsetPose();
             viewer.frame();
             
             if (textureTarget->getTextureObject(0))
             {
-                vrInstance->SubmitFrame(textureTarget, textureTarget);
+                //vrInstance->SubmitFrame(textureTarget, textureTarget);
+				vrInstance->SubmitFrame(test, test);
             }
         }
         
