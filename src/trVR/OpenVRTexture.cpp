36ad77c1d80cf0d0288fa5caa5ce2982c6bbd9b1
Alexander Koufos <exoticdft@gmail.com>
2018-09-17 23:22:18 -0400

    Issue #18 Cleans up code
    - Removes some unneeded code from library classes.

--- src/trVR/OpenVRTexture.cpp
+++ src/trVR/OpenVRTexture.cpp
@@ -84,7 +84,6 @@ namespace trVR
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, test);
             
             // check FBO status
