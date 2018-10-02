cca3b933b8ab17f955ae6824420ca6c93e59ce36
DieSlower <maximster@gmail.com>
2018-10-02 15:27:29 -0400

    Issue #35 Adds the initial code for the Node wrapper

--- include/trSG/Node.h
+++ include/trSG/Node.h
@@ -21,23 +21,32 @@
 
 #pragma once
 
-#include <trSG/Export.h>"
+#include <trSG/Export.h>
 
-#include <trBase/UserDataContainer.h>
-#include <trBase/SmrtPtr.h>
 #include <trBase/Base.h>
+#include <trBase/SmrtPtr.h>
 #include <trUtil/RefStr.h>
 
-#include <osg/Node>
+namespace trBase
+{
+    class UserDataContainer;
+}
+
+namespace osg
+{
+    class Group;
+    class Node;
+    class NodeVisitor;
+}
 
 namespace trSG
 {
     
     /**
-    * Base class for creating all the scenegraphs nodes.
+    * Class for wrapping the osg node
     * Internally it contains an OSG Node, that can be accessed by user if additional functionality is required. 
     */
-    class TR_SG_EXPORT NodeBase : public trBase::Base
+    class TR_SG_EXPORT Node : public trBase::Base
     {
     public:
 
@@ -46,74 +55,84 @@ namespace trSG
         const static trUtil::RefStr CLASS_TYPE;         /// Holds the class type name for efficient comparisons
 
         /**
-        * This is a set of bits (flags) that represent the Node.
-        * The default value is 0xffffffff (all bits set).
-        *
-        * The most common use of these is during traversal of the scene graph.
-        *
-        * For example, if a Node has a NodeMask value of 0x02 (only 2nd bit set)
-        * and the Camera has a CullMask of 0x4 (2nd bit not set) then during cull traversal,
-        * which takes it's TraversalMask from the Camera's CullMask, the node and any children
-        * would be ignored and thereby treated as "culled" and thus not rendered.
-        * Conversely, if the Camera CullMask were 0x3 (2nd bit set) then the node
-        * would be processed and child Nodes would be examined.
-        */
-        using NodeMask = osg::Node::NodeMask;
+         * @brief   This is a set of bits (flags) that represent the Node. The default value is
+         *          0xffffffff (all bits set).
+         *          
+         *          The most common use of these is during traversal of the scene graph.
+         *          
+         *          For example, if a Node has a NodeMask value of 0x02 (only 2nd bit set)
+         *          and the Camera has a CullMask of 0x4 (2nd bit not set) then during cull traversal,
+         *          which takes it's TraversalMask from the Camera's CullMask, the node and any children
+         *          would be ignored and thereby treated as "culled" and thus not rendered. Conversely,
+         *          if the Camera CullMask were 0x3 (2nd bit set) then the node would be processed and
+         *          child Nodes would be examined.
+         */
+        using NodeMask = unsigned int;
 
         /**
-        * ctor
-        */
-        NodeBase(const std::string name = CLASS_TYPE);
+         * @fn  Node::Node(const std::string name = CLASS_TYPE);
+         *
+         * @brief   Constructor.
+         *
+         * @param   name    (Optional) The name.
+         */
+        Node(const std::string name = CLASS_TYPE);
 
         /**
-        * Returns the class type
-        */
+         * @fn  virtual const std::string& Node::GetType() const override;
+         *
+         * @brief   Returns the class type.
+         *
+         * @return  The type.
+         */
         virtual const std::string& GetType() const override;
 
         /**
-        * Sets this instances name
-        */
+         * @fn  virtual void Node::SetName(const std::string& name);
+         *
+         * @brief   Sets this instances name.
+         *
+         * @param   name    The name.
+         */
         virtual void SetName(const std::string& name);
 
         /**
-        * Returns a pointer to the internal OSG Node
-        */
+         * @fn  virtual osg::Node* Node::AsOSGNode();
+         *
+         * @brief   Returns a pointer to the internal OSG Node.
+         *
+         * @return  Null if it fails, else a pointer to an osg::Node.
+         */
         virtual osg::Node* AsOSGNode();
-        
+
         /**
-        * Returns a constant pointer to the internal OSG Node
-        */
+         * @fn  virtual const osg::Node* Node::AsOSGNode() const;
+         *
+         * @brief   Returns a constant pointer to the internal OSG Node.
+         *
+         * @return  Null if it fails, else a pointer to a const osg::Node.
+         */
         virtual const osg::Node* AsOSGNode() const;
 
-        /**
-        * Returns the custom user data that could be stored in the class instance
-        * Note, that if the user did not set any data, a NULL will be returned
-        */
-        virtual trBase::UserDataContainer* GetUserData();
-
-        /**
-        * Returns the custom user data that could be stored in the class instance
-        * Note, that if the user did not set any data, a NULL will be returned
-        */
-        virtual const trBase::UserDataContainer* GetUserData() const;
-
-        /**
-        * Sets the user data for the current instance 
-        */
-        virtual void SetUserData(trBase::UserDataContainer* data);
+        /** Visitor Pattern : calls the apply method of a NodeVisitor with this node's type.*/
+        //virtual void accept(NodeVisitor& nv);
+        
+        /** Traverse upwards : calls parents' accept method with NodeVisitor.*/
+        //virtual void ascend(NodeVisitor& nv);
+        
+        /** Traverse downwards : calls children's accept method with NodeVisitor.*/
+        //virtual void traverse(NodeVisitor& /*nv*/) {}
 
     protected:
 
         trBase::SmrtPtr<osg::Node> mNode;
 
         /**
-        * dtor
-        */
-        ~NodeBase();
-
-    private:
-
-        trBase::SmrtPtr<trBase::UserDataContainer> mUserData;
+         * @fn  Node::~Node();
+         *
+         * @brief   dtor.
+         */
+        ~Node();
     };
 }
 
--- src/trSG/Node.cpp
+++ src/trSG/Node.cpp
@@ -21,63 +21,49 @@
 
 #include <trSG/Node.h>
 
+#include <trBase/Base.h>
+
+#include <osg/Node>
+
 namespace trSG
 {
-    const trUtil::RefStr NodeBase::CLASS_TYPE = trUtil::RefStr("trSG::Node");
+    const trUtil::RefStr Node::CLASS_TYPE = trUtil::RefStr("trSG::Node");
 
     //////////////////////////////////////////////////////////////////////////
-    NodeBase::NodeBase(const std::string name) : BaseClass(name)
+    Node::Node(const std::string name) : BaseClass(name)
     {
         mNode = new osg::Node(); //Creates our internal OSG node. 
         mNode->setName(name);
     }
 
     //////////////////////////////////////////////////////////////////////////
-    NodeBase::~NodeBase()
+    Node::~Node()
     {
     }
 
     //////////////////////////////////////////////////////////////////////////
-    const std::string& NodeBase::GetType() const
+    const std::string& Node::GetType() const
     {
         return CLASS_TYPE;
     }
 
     //////////////////////////////////////////////////////////////////////////
-    void NodeBase::SetName(const std::string& name)
+    void Node::SetName(const std::string& name)
     {
         BaseClass::SetName(name);
         mNode->setName(name);
     }
 
     //////////////////////////////////////////////////////////////////////////
-    osg::Node* NodeBase::AsOSGNode()
+    osg::Node* Node::AsOSGNode()
     {
         return mNode.Get();
     }
 
     //////////////////////////////////////////////////////////////////////////
-    const osg::Node* NodeBase::AsOSGNode() const
+    const osg::Node* Node::AsOSGNode() const
     {
         return mNode.Get();
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    trBase::UserDataContainer* NodeBase::GetUserData()
-    {
-        return mUserData;
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    const trBase::UserDataContainer* NodeBase::GetUserData() const
-    {
-        return mUserData;
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    void NodeBase::SetUserData(trBase::UserDataContainer* data)
-    {
-        mUserData = data;
     }
 
     //////////////////////////////////////////////////////////////////////////
