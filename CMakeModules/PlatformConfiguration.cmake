8a6bb2f6d42d777c8aea8a0d6a5ce894eb287930
DieSlower <maximster@gmail.com>
2018-11-15 00:20:39 -0500

    Issue #86 Converts the project to using C++17 standard

--- CMakeModules/PlatformConfiguration.cmake
+++ CMakeModules/PlatformConfiguration.cmake
@@ -2,9 +2,9 @@
 # Common global definitions.***************************************************
 # *****************************************************************************
 
-# Require C++14 support, prefer ISO C++ over GNU variants,
+# Require C++17 support, prefer ISO C++ over GNU variants,
 # as relying solely on ISO C++ is more portable.
-SET (CMAKE_CXX_STANDARD 14)
+SET (CMAKE_CXX_STANDARD 17)
 SET (CMAKE_CXX_STANDARD_REQUIRED ON)
 SET (CMAKE_CXX_EXTENSIONS OFF)
 
@@ -128,12 +128,6 @@ IF (WIN32)
             SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DBOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE") #Boost has a bug with using an old config file with VS2017
             SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DBOOST_ALL_DYN_LINK")                     #Force only boost dynamic libraries to be used
             LINK_DIRECTORIES (${LINK_DIRECTORIES} ${Boost_LIBRARY_DIRS})                        #Force all projects to have boost dirs as aditional library paths
-        ENDIF ()
-
-        #If we are using Google Test
-        IF (GoogleTest_FOUND)
-            #Sets temporary warning suppression flags. These should be revisited and rechecked at every VS version and Ext dependency change. 
-            SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING")     #GTest uses old TR1 Namespaces 
         ENDIF ()
     ENDIF ()
 ENDIF ()
