8486c83da7a961dd9ba6aa1f3e1c1db2553c79ae
DieSlower <maximster@gmail.com>
2018-11-19 21:34:36 -0500

    Issue #85 Updates namespace notation in the LogWriterFile class

--- include/trUtil/Logging/LogWriterFile.h
+++ include/trUtil/Logging/LogWriterFile.h
@@ -38,92 +38,82 @@
  *
  * @brief   .
  */
-namespace trUtil
+namespace trUtil::Logging
 {
-/**
- * @namespace   Logging
- *
- * @brief   .
- */
-    namespace Logging
+    /**
+        * @class   LogWriterFile
+        *
+        * @brief   A specialized LogWriter class that outputs Log messages to a file.
+        */
+    class TR_UTIL_EXPORT LogWriterFile : public Logging::LogWriter
     {
+    public:
+
         /**
-         * @class   LogWriterFile
-         *
-         * @brief   A specialized LogWriter class that outputs Log messages to a file.
-         */
-        class TR_UTIL_EXPORT LogWriterFile : public Logging::LogWriter
-        {
-        public:
+            * @brief   The folder path that is appended to the User Data folder
+            *          (PathUtil::GetUserTCDataFolder())
+            */
+        const static std::string DEFAULT_LOG_FOLDER;
 
-            /**
-             * @brief   The folder path that is appended to the User Data folder
-             *          (PathUtil::GetUserTCDataFolder())
-             */
-            const static std::string DEFAULT_LOG_FOLDER;
+        /**
+            * @fn  LogWriterFile::LogWriterFile();
+            *
+            * @brief   Default constructor.
+            */
+        LogWriterFile();
 
-            /**
-             * @fn  LogWriterFile::LogWriterFile();
-             *
-             * @brief   Default constructor.
-             */
-            LogWriterFile();
+        /**
+            * @fn  void LogWriterFile::OpenFile();
+            *
+            * @brief   Opens the file.
+            */
+        void OpenFile();
 
-            /**
-             * @fn  void LogWriterFile::OpenFile();
-             *
-             * @brief   Opens the file.
-             */
-            void OpenFile();
+        /**
+            * @fn  virtual void LogWriterFile::LogMessage(const LogData& logData);
+            *
+            * @brief   Logs a message.
+            *
+            * @param   logData Information describing the log.
+            */
+        virtual void LogMessage(const LogData& logData);
 
-            /**
-             * @fn  virtual void LogWriterFile::LogMessage(const LogData& logData);
-             *
-             * @brief   Logs a message.
-             *
-             * @param   logData Information describing the log.
-             */
-            virtual void LogMessage(const LogData& logData);
+        /**
+            * @fn  void LogWriterFile::LogHorizRule();
+            *
+            * @brief   Logs horiz rule.
+            */
+        void LogHorizRule();
 
-            /**
-             * @fn  void LogWriterFile::LogHorizRule();
-             *
-             * @brief   Logs horiz rule.
-             */
-            void LogHorizRule();
+        /**
+            * @fn  bool LogWriterFile::IsOpenFailed();
+            *
+            * @brief   Returns true if opening a file failed.
+            *
+            * @return  True if open failed, false if not.
+            */
+        bool IsOpenFailed();
 
-            /**
-             * @fn  bool LogWriterFile::IsOpenFailed();
-             *
-             * @brief   Returns true if opening a file failed.
-             *
-             * @return  True if open failed, false if not.
-             */
-            bool IsOpenFailed();
+        /**
+            * @fn  void LogWriterFile::ResetOpenFail();
+            *
+            * @brief   A utility function to reset the OpenFailed flag after a file failure.
+            */
+        void ResetOpenFail();
 
-            /**
-             * @fn  void LogWriterFile::ResetOpenFail();
-             *
-             * @brief   A utility function to reset the OpenFailed flag after a file failure.
-             */
-            void ResetOpenFail();
+    protected:
 
-        protected:
+        /**
+            * @fn  virtual LogWriterFile::~LogWriterFile();
+            *
+            * @brief   Destructor.
+            */
+        virtual ~LogWriterFile();
 
-            /**
-             * @fn  virtual LogWriterFile::~LogWriterFile();
-             *
-             * @brief   Destructor.
-             */
-            virtual ~LogWriterFile();
-
-        private:
-            std::ofstream mLogFile;
-            bool mOpenFailed;
-            void TimeTag(std::string prefix);
-            void EndFile();
-        };
-    }
+    private:
+        std::ofstream mLogFile;
+        bool mOpenFailed;
+        void TimeTag(std::string prefix);
+        void EndFile();
+    };
 }
-
-
