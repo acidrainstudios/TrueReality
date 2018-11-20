6d8cd2a63c666105703c8e5815b9332010f2fb75
DieSlower <maximster@gmail.com>
2018-11-19 21:32:13 -0500

    Issue #85 Updated the namespace notation in the Log class

--- src/trUtil/Logging/Log.cpp
+++ src/trUtil/Logging/Log.cpp
@@ -41,307 +41,304 @@
 #include <algorithm>
 #include <stdarg.h>
 
-namespace trUtil
+namespace trUtil::Logging
 {
-    namespace Logging
+    static osg::ref_ptr<LogManager> LOG_MANAGER(NULL);
+    static LogLevel DEFAULT_LOG_LEVEL(LogLevel::LOG_WARNING);
+
+    //////////////////////////////////////////////////////////////////////////
+    class LogImpl //: std::stringbuf
     {
-        static osg::ref_ptr<LogManager> LOG_MANAGER(NULL);
-        static LogLevel DEFAULT_LOG_LEVEL(LogLevel::LOG_WARNING);
-
-        //////////////////////////////////////////////////////////////////////////
-        class LogImpl //: std::stringbuf
-        {
-        public:
-            explicit LogImpl(const std::string& name)
-                : mOutputStreamBit(Log::STANDARD)
-                , mName(name)
-                , mLevel(DEFAULT_LOG_LEVEL)
-                , mWriters()
-            {
-            }
-
-            unsigned int mOutputStreamBit; ///<the current output stream option
-            std::string mName;
-            LogLevel mLevel;
-            Log::LogWriterContainer mWriters;
-        };
-
-        //////////////////////////////////////////////////////////////////////////
-        const std::string Log::LOG_DEFAULT_NAME("MainLog");
-
-        //////////////////////////////////////////////////////////////////////////
-        Log::Log(const std::string& name)
-            :
-            mImpl(new LogImpl(name))
+    public:
+        explicit LogImpl(const std::string& name)
+            : mOutputStreamBit(Log::STANDARD)
+            , mName(name)
+            , mLevel(DEFAULT_LOG_LEVEL)
+            , mWriters()
         {
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        Log::~Log()
+        unsigned int mOutputStreamBit; ///<the current output stream option
+        std::string mName;
+        LogLevel mLevel;
+        Log::LogWriterContainer mWriters;
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+    const std::string Log::LOG_DEFAULT_NAME("MainLog");
+
+    //////////////////////////////////////////////////////////////////////////
+    Log::Log(const std::string& name)
+        :
+        mImpl(new LogImpl(name))
+    {
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    Log::~Log()
+    {
+        delete mImpl;
+        mImpl = nullptr;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogMessage(const std::string& cppFile, const std::string& method, int line, const std::string& msg, LogLevel logLevel) const
+    {
+        if (mImpl->mOutputStreamBit == Log::NO_OUTPUT)
         {
-            delete mImpl;
-            mImpl = nullptr;
+            return;
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogMessage(const std::string& cppFile, const std::string& method, int line, const std::string& msg, LogLevel logLevel) const
+        if (logLevel < mImpl->mLevel)
         {
-            if (mImpl->mOutputStreamBit == Log::NO_OUTPUT)
+            return;
+        }
+
+
+        OpenThreads::ScopedLock<OpenThreads::Mutex> lock(LOG_MANAGER->GetMutex());
+        bool hasLogTimeProvider = LOG_MANAGER->IsLogTimeProviderValid();
+
+        LogWriter::LogData logData;
+        if (hasLogTimeProvider)
+        {
+            logData.frameNumber = LOG_MANAGER->GetFrameNumber();
+            logData.time = LOG_MANAGER->GetDateTime();
+        }
+        else
+        {
+            logData.time.SetToLocalTime();
+        }
+
+        logData.logLevel = logLevel;
+        logData.logName = mImpl->mName;
+        logData.file = osgDB::getSimpleFileName(cppFile);
+        logData.method = method;
+        logData.line = line;
+        logData.msg = msg;
+
+
+        if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_FILE))
+        {
+            LOG_MANAGER->LogMessageToFile(logData);
+        }
+
+        if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_CONSOLE))
+        {
+            LOG_MANAGER->LogMessageToConsole(logData);
+        }
+
+        if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_WRITER) && !mImpl->mWriters.empty())
+        {
+            Log::LogWriterContainer::iterator itr = mImpl->mWriters.begin();
+            while (itr != mImpl->mWriters.end())
             {
-                return;
-            }
-
-            if (logLevel < mImpl->mLevel)
-            {
-                return;
-            }
-
-
-            OpenThreads::ScopedLock<OpenThreads::Mutex> lock(LOG_MANAGER->GetMutex());
-            bool hasLogTimeProvider = LOG_MANAGER->IsLogTimeProviderValid();
-
-            LogWriter::LogData logData;
-            if (hasLogTimeProvider)
-            {
-                logData.frameNumber = LOG_MANAGER->GetFrameNumber();
-                logData.time = LOG_MANAGER->GetDateTime();
-            }
-            else
-            {
-                logData.time.SetToLocalTime();
-            }
-
-            logData.logLevel = logLevel;
-            logData.logName = mImpl->mName;
-            logData.file = osgDB::getSimpleFileName(cppFile);
-            logData.method = method;
-            logData.line = line;
-            logData.msg = msg;
-
-
-            if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_FILE))
-            {
-                LOG_MANAGER->LogMessageToFile(logData);
-            }
-
-            if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_CONSOLE))
-            {
-                LOG_MANAGER->LogMessageToConsole(logData);
-            }
-
-            if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_WRITER) && !mImpl->mWriters.empty())
-            {
-                Log::LogWriterContainer::iterator itr = mImpl->mWriters.begin();
-                while (itr != mImpl->mWriters.end())
-                {
-                    (*itr)->LogMessage(logData);
-                    ++itr;
-                }
+                (*itr)->LogMessage(logData);
+                ++itr;
             }
         }
+    }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const char* msg, va_list list) const
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const char* msg, va_list list) const
+    {
+        char buffer[2049];
+
+        vsnprintf(buffer, 2049, msg, list);
+
+        LogMessage("", source, line, buffer, logLevel);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogMessage(LogLevel logLevel, const std::string& source, const char* msg, ...) const
+    {
+        va_list list;
+
+        va_start(list, msg);
+        LogMessage(logLevel, source, -1, msg, list);
+        va_end(list);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const char* msg, ...) const
+    {
+        va_list list;
+
+        va_start(list, msg);
+        LogMessage(logLevel, source, line, msg, list);
+        va_end(list);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const std::string& msg) const
+    {
+        LogMessage("", source, line, msg, logLevel);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::LogHorizRule()
+    {
+        if (mImpl->mOutputStreamBit == Log::NO_OUTPUT)
         {
-            char buffer[2049];
-
-            vsnprintf(buffer, 2049, msg, list);
-
-            LogMessage("", source, line, buffer, logLevel);
+            return;
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogMessage(LogLevel logLevel, const std::string& source, const char* msg, ...) const
+        if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_FILE))
         {
-            va_list list;
+            LOG_MANAGER->LogHorizRule();
+        }
+    }
 
-            va_start(list, msg);
-            LogMessage(logLevel, source, -1, msg, list);
-            va_end(list);
+    //////////////////////////////////////////////////////////////////////////
+    Log& Log::GetInstance(const std::string& name)
+    {
+        if (LOG_MANAGER == nullptr)
+        {
+            LOG_MANAGER = new LogManager;
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const char* msg, ...) const
+        Log* l = LOG_MANAGER->GetInstance(name);
+        if (l == nullptr)
         {
-            va_list list;
-
-            va_start(list, msg);
-            LogMessage(logLevel, source, line, msg, list);
-            va_end(list);
+            l = new Log(name);
+            LOG_MANAGER->AddInstance(name, l);
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogMessage(LogLevel logLevel, const std::string& source, int line, const std::string& msg) const
-        {
-            LogMessage("", source, line, msg, logLevel);
-        }
+        return *l;
+    }
 
-        //////////////////////////////////////////////////////////////////////////
-        void Log::LogHorizRule()
+    //////////////////////////////////////////////////////////////////////////
+    LogManager& Log::GetLogManagerRef()
+    {
+        return *LOG_MANAGER;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    void Log::SetDefaultLogLevel(LogLevel newLevel)
+    {
+        DEFAULT_LOG_LEVEL = newLevel;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    const std::string Log::GetLogLevelString(LogLevel logLevel) //static
+    {
+        return LogLevelToString(logLevel);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    LogLevel Log::GetLogLevelForString(const std::string& levelString)  //static
+    {
+        return LogLevelFromString(levelString);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    void Log::SetOutputStreamBit(unsigned int option)
+    {
+        mImpl->mOutputStreamBit = option;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    void Log::SetAllOutputStreamBits(unsigned int option)
+    {
+        if (LOG_MANAGER.valid())
         {
-            if (mImpl->mOutputStreamBit == Log::NO_OUTPUT)
+            LOG_MANAGER->SetAllOutputStreamBits(option);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    unsigned int Log::GetOutputStreamBit() const
+    {
+        return mImpl->mOutputStreamBit;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    const std::string& Log::GetName() const
+    {
+        return mImpl->mName;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    bool Log::IsLevelEnabled(LogLevel logLevel) const
+    {
+        return logLevel >= mImpl->mLevel;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    void Log::SetLogLevel(LogLevel logLevel)
+    {
+        mImpl->mLevel = logLevel;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    LogLevel Log::GetLogLevel() const
+    {
+        return mImpl->mLevel;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    void Log::SetAllLogLevels(LogLevel newLevel) //static
+    {
+        if (LOG_MANAGER.valid())
+        {
+            LOG_MANAGER->SetAllLogLevels(newLevel);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    void Log::SetLogTimeProvider(LogTimeProvider* ltp)
+    {
+        if (LOG_MANAGER.valid())
+        {
+            LOG_MANAGER->SetLogTimeProvider(ltp);
+
+            if (ltp != NULL && !LOG_MANAGER->IsLogTimeProviderValid())
             {
-                return;
-            }
-
-            if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_FILE))
-            {
-                LOG_MANAGER->LogHorizRule();
-            }
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        Log& Log::GetInstance(const std::string& name)
-        {
-            if (LOG_MANAGER == nullptr)
-            {
-                LOG_MANAGER = new LogManager;
-            }
-
-            Log* l = LOG_MANAGER->GetInstance(name);
-            if (l == nullptr)
-            {
-                l = new Log(name);
-                LOG_MANAGER->AddInstance(name, l);
-            }
-
-            return *l;
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        LogManager& Log::GetLogManagerRef()
-        {
-            return *LOG_MANAGER;
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        void Log::SetDefaultLogLevel(LogLevel newLevel)
-        {
-            DEFAULT_LOG_LEVEL = newLevel;
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        const std::string Log::GetLogLevelString(LogLevel logLevel) //static
-        {
-            return LogLevelToString(logLevel);
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        LogLevel Log::GetLogLevelForString(const std::string& levelString)  //static
-        {
-            return LogLevelFromString(levelString);
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        void Log::SetOutputStreamBit(unsigned int option)
-        {
-            mImpl->mOutputStreamBit = option;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        void Log::SetAllOutputStreamBits(unsigned int option)
-        {
-            if (LOG_MANAGER.valid())
-            {
-                LOG_MANAGER->SetAllOutputStreamBits(option);
-            }
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        unsigned int Log::GetOutputStreamBit() const
-        {
-            return mImpl->mOutputStreamBit;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        const std::string& Log::GetName() const
-        {
-            return mImpl->mName;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        bool Log::IsLevelEnabled(LogLevel logLevel) const
-        {
-            return logLevel >= mImpl->mLevel;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        void Log::SetLogLevel(LogLevel logLevel)
-        {
-            mImpl->mLevel = logLevel;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        LogLevel Log::GetLogLevel() const
-        {
-            return mImpl->mLevel;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        void Log::SetAllLogLevels(LogLevel newLevel) //static
-        {
-            if (LOG_MANAGER.valid())
-            {
-                LOG_MANAGER->SetAllLogLevels(newLevel);
-            }
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        void Log::SetLogTimeProvider(LogTimeProvider* ltp)
-        {
-            if (LOG_MANAGER.valid())
-            {
-                LOG_MANAGER->SetLogTimeProvider(ltp);
-
-                if (ltp != NULL && !LOG_MANAGER->IsLogTimeProviderValid())
-                {
-                    LOG_E("Unable to assign log time provider because it did not provide a referenced object as a key for deletion");
-                    LOG_MANAGER->SetLogTimeProvider(NULL);
-                }
-            }
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        void Log::AddWriter(LogWriter& writer)
-        {
-            mImpl->mWriters.push_back(&writer);
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        const Log::LogWriterContainer& Log::GetWriters() const
-        {
-            return mImpl->mWriters;
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        Log::LogWriterContainer& Log::GetWriters()
-        {
-            return mImpl->mWriters;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        void Log::RemoveWriter(LogWriter& writer)
-        {
-            LogWriterContainer::iterator found = std::find(mImpl->mWriters.begin(),
-                mImpl->mWriters.end(), &writer);
-            if (found != mImpl->mWriters.end())
-            {
-                mImpl->mWriters.erase(found);
+                LOG_E("Unable to assign log time provider because it did not provide a referenced object as a key for deletion");
+                LOG_MANAGER->SetLogTimeProvider(NULL);
             }
         }
+    }
 
-        ////////////////////////////////////////////////////////////////////////////////
-        LoggingOff::LoggingOff(const std::string& name)
-            : mLog(trUtil::Logging::Log::GetInstance(name))
+    ////////////////////////////////////////////////////////////////////////////////
+    void Log::AddWriter(LogWriter& writer)
+    {
+        mImpl->mWriters.push_back(&writer);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    const Log::LogWriterContainer& Log::GetWriters() const
+    {
+        return mImpl->mWriters;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    Log::LogWriterContainer& Log::GetWriters()
+    {
+        return mImpl->mWriters;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    void Log::RemoveWriter(LogWriter& writer)
+    {
+        LogWriterContainer::iterator found = std::find(mImpl->mWriters.begin(),
+            mImpl->mWriters.end(), &writer);
+        if (found != mImpl->mWriters.end())
         {
-            mOldLevel = mLog.GetLogLevel();
-            // Only ALWAYS log levels are on.
-            mLog.SetLogLevel(trUtil::Logging::LogLevel::LOG_ALWAYS);
+            mImpl->mWriters.erase(found);
         }
-        ////////////////////////////////////////////////////////////////////////////////
-        LoggingOff::~LoggingOff()
-        {
-            mLog.SetLogLevel(mOldLevel);
-        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    LoggingOff::LoggingOff(const std::string& name)
+        : mLog(trUtil::Logging::Log::GetInstance(name))
+    {
+        mOldLevel = mLog.GetLogLevel();
+        // Only ALWAYS log levels are on.
+        mLog.SetLogLevel(trUtil::Logging::LogLevel::LOG_ALWAYS);
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    LoggingOff::~LoggingOff()
+    {
+        mLog.SetLogLevel(mOldLevel);
     }
 }
