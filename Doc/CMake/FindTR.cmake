c51529b42a86a0900871f66bb904ea0b9a56bc8b
DieSlower <maximster@gmail.com>
2018-11-27 21:23:42 -0500

    Issue #25 Changes the file header to comply with the general CMake license

--- Doc/CMake/FindTR.cmake
+++ Doc/CMake/FindTR.cmake
@@ -1,21 +1,7 @@
-﻿# True Reality Open Source Game and Simulation Engine
-# Copyright © 2018 Acid Rain Studios LLC
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
 #
-# This library is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 3.0 of the License, or (at your option)
-# any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
-# @author Maxim Serebrennik
+# @author Acid Rain Studios LLC
 
 
 # ********************************************************************************
