3be21ed4dc9ec72c4b3e7890796c52be26657e75
DieSlower <maximster@gmail.com>
2019-01-01 22:53:59 -0500

    Issue #91 Adds initial code for the Node class

--- include/trCore/Nodes/Node.h
+++ include/trCore/Nodes/Node.h
@@ -23,7 +23,119 @@
 
 #include <trCore/Export.h>
 
+#include <trBase/Base.h>
+#include <trBase/SmrtPtr.h>
+#include <trUtil/RefStr.h>
+
+namespace trBase
+{
+    class UserDataContainer;
+}
+
+namespace osg
+{
+    class Group;
+    class Node;
+    class NodeVisitor;
+}
+
 namespace trCore::Nodes
 {
-    
+    /**
+     * @class   Node
+     *
+     * @author Maxim Serebrennik
+     *
+     * @brief   Class for wrapping the osg node Internally it contains an OSG Group, that can be
+     *          accessed by user if additional functionality is required.
+     */
+    class TR_CORE_EXPORT Node : public trBase::Base
+    {
+    public:
+
+        using BaseClass = trBase::Base;                 /// Adds an easy and swappable access to the base class
+
+        const static trUtil::RefStr CLASS_TYPE;         /// Holds the class type name for efficient comparisons
+
+        /**
+         * @brief   This is a set of bits (flags) that represent the Node. The default value is
+         *          0xffffffff (all bits set).
+         *          
+         *          The most common use of these is during traversal of the scene graph.
+         *          
+         *          For example, if a Node has a NodeMask value of 0x02 (only 2nd bit set)
+         *          and the Camera has a CullMask of 0x4 (2nd bit not set) then during cull traversal,
+         *          which takes it's TraversalMask from the Camera's CullMask, the node and any children
+         *          would be ignored and thereby treated as "culled" and thus not rendered. Conversely,
+         *          if the Camera CullMask were 0x3 (2nd bit set) then the node would be processed and
+         *          child Nodes would be examined.
+         */
+        using NodeMask = unsigned int;
+
+        /**
+         * @fn  Node::Node(const std::string name = CLASS_TYPE);
+         *
+         * @brief   Constructor.
+         *
+         * @param   name    (Optional) The name.
+         */
+        Node(const std::string name = CLASS_TYPE);
+
+        /**
+         * @fn  virtual const std::string& Node::GetType() const override;
+         *
+         * @brief   Returns the class type.
+         *
+         * @return  The type.
+         */
+        virtual const std::string& GetType() const override;
+
+        /**
+         * @fn  virtual void Node::SetName(const std::string& name);
+         *
+         * @brief   Sets this instances name.
+         *
+         * @param   name    The name.
+         */
+        virtual void SetName(const std::string& name);
+
+        /**
+         * @fn  virtual osg::Node* Node::AsOSGNode();
+         *
+         * @brief   Returns a pointer to the internal OSG Node.
+         *
+         * @return  Null if it fails, else a pointer to an osg::Node.
+         */
+        virtual osg::Node* AsOSGNode();
+
+        /**
+         * @fn  virtual const osg::Node* Node::AsOSGNode() const;
+         *
+         * @brief   Returns a constant pointer to the internal OSG Node.
+         *
+         * @return  Null if it fails, else a pointer to a const osg::Node.
+         */
+        virtual const osg::Node* AsOSGNode() const;
+
+        /** Visitor Pattern : calls the apply method of a NodeVisitor with this node's type.*/
+        //virtual void accept(NodeVisitor& nv);
+
+        /** Traverse upwards : calls parents' accept method with NodeVisitor.*/
+        //virtual void ascend(NodeVisitor& nv);
+
+        /** Traverse downwards : calls children's accept method with NodeVisitor.*/
+        //virtual void traverse(NodeVisitor& /*nv*/) {}
+
+    protected:
+
+        /** @brief   The node. */
+        trBase::SmrtPtr<osg::Group> mNode;
+
+        /**
+         * @fn  Node::~Node();
+         *
+         * @brief   dtor.
+         */
+        ~Node();
+    };
 }
