0320c3bf10e91fb45736e4957706f48227517541
DieSlower <maximster@gmail.com>
2019-01-19 16:10:53 -0500

    Issue #62 Fixes some file headers

--- src/trUtil/PathUtils.cpp
+++ src/trUtil/PathUtils.cpp
@@ -1,6 +1,6 @@
 /*
 * True Reality Open Source Game and Simulation Engine
-* Copyright � 2018 Acid Rain Studios LLC
+* Copyright © 2019 Acid Rain Studios LLC
 *
 * The Base of this class has been adopted from the Delta3D engine
 *
