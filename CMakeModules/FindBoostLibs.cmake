aa1da8cb247ae0cdd66ede6c8d339ff1473fce6c
DieSlower <maximster@gmail.com>
2019-03-11 18:21:06 -0400

    Issue #100 Removes dependency on most of the Boost Libraries

--- CMakeModules/FindBoostLibs.cmake
+++ CMakeModules/FindBoostLibs.cmake
@@ -70,39 +70,7 @@
 
 SET (DEPENDENCY "BoostLibs")
 
-FIND_PACKAGE (Boost ${BoostLibs_FIND_VERSION} REQUIRED COMPONENTS 
-    #atomic 
-    chrono 
-    #container 
-    #context 
-    #coroutine 
-    date_time         
-    #filesystem 
-    #graph 
-    #iostreams 
-    #locale 
-    #log_setup 
-    #log        
-    #math_c99f 
-    #math_c99l 
-    #math_c99 
-    #math_tr1f 
-    #math_tr1l 
-    #math_tr1         
-    #prg_exec_monitor 
-    program_options 
-    #random 
-    #regex 
-    #serialization 
-    #signals 
-    system 
-    thread 
-    #timer 
-    #type_erasure 
-    unit_test_framework 
-    #wave 
-    #wserialization        
-    )
+FIND_PACKAGE (Boost ${BoostLibs_FIND_VERSION} REQUIRED )
 
 SET (${DEPENDENCY}_FOUND "NO")
 IF (Boost_FOUND)
