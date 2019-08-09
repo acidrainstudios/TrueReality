e3cfd108d7e2c3543e1fa448ba2f87c9a73ba15a
DieSlower <maximster@gmail.com>
2019-08-08 22:42:21 -0400

    Issue #114 Exchanges the =(const std::string& value);  operator for =(const RefStr& value);

--- include/trUtil/RefStr.h
+++ include/trUtil/RefStr.h
@@ -112,7 +112,7 @@ namespace trUtil
          */
         operator const char*() const { return mString->c_str(); }
         
-        trUtil::RefStr& operator=(const RefStr& value);
+        trUtil::RefStr& operator=(const std::string& value);
 
         RefStr operator+(const std::string& string) const;
         RefStr operator+(const RefStr& RefStr) const;
