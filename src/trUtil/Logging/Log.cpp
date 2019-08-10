056ab1832b42c3fd096328cef0a46d40d36f1edc
DieSlower <maximster@gmail.com>
2019-08-10 00:43:49 -0400

    Issue #115 Adds the copying of log data when testing is enabled

--- src/trUtil/Logging/Log.cpp
+++ src/trUtil/Logging/Log.cpp
@@ -1,6 +1,6 @@
 /*
 * True Reality Open Source Game and Simulation Engine
-* Copyright ï¿½ 2019 Acid Rain Studios LLC
+* Copyright © 2019 Acid Rain Studios LLC
 *
 * The Base of this class has been adopted from the Delta3D engine
 *
@@ -115,6 +115,11 @@ namespace trUtil::Logging
         logData.line = line;
         logData.msg = msg;
 
+        // If testing is enabled, copy the logg data to an accessible variable
+        if (mTestingMode)
+        {
+            mLogTestData = logData;
+        }
 
         if (trUtil::Bits::Has(mImpl->mOutputStreamBit, Log::TO_FILE))
         {
