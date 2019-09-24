25ed38e8b6f7e9c40c4de91890f6619b4213d6da
DieSlower <maximster@gmail.com>
2019-09-24 00:40:05 -0400

    Issue #109 Makes the > operator use mGUIDPtr

--- src/trBase/UniqueId.cpp
+++ src/trBase/UniqueId.cpp
@@ -370,7 +370,7 @@ namespace trBase
     ////////////////////////////////////////////////
     bool UniqueId::operator>(const UniqueId & id) const
     {
-        return mGUID > id.mGUID;
+        return mGUIDPtr->operator> (*id.mGUIDPtr);
     }
 
     ////////////////////////////////////////////////
