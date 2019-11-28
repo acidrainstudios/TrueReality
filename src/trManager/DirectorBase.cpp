0f3896c1cc46e6a9d556cd619a6a20a125abfced
DieSlower <maximster@gmail.com>
2019-11-27 21:47:46 -0500

    Issue #132 Removes an unused parameter

--- src/trManager/DirectorBase.cpp
+++ src/trManager/DirectorBase.cpp
@@ -1,6 +1,6 @@
 /*
 * True Reality Open Source Game and Simulation Engine
-* Copyright © 2019 Acid Rain Studios LLC
+* Copyright ï¿½ 2019 Acid Rain Studios LLC
 *
 * This library is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Lesser General Public License as published by the Free
@@ -67,7 +67,7 @@ namespace trManager
     }
 
     //////////////////////////////////////////////////////////////////////////
-    void DirectorBase::OnTick(const trManager::MessageBase& msg)
+    void DirectorBase::OnTick(const trManager::MessageBase& /*msg*/)
     {
         //Do Nothing
     }
