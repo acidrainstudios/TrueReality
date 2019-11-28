623f1b2e0da160b65fef6ff8bdf082218edca63f
DieSlower <maximster@gmail.com>
2019-11-27 21:57:04 -0500

    Issue #132 Removes an unused parameters

--- src/trStart/Main.cpp
+++ src/trStart/Main.cpp
@@ -1,6 +1,6 @@
 /*
 * True Reality Open Source Game and Simulation Engine
-* Copyright © 2019 Acid Rain Studios LLC
+* Copyright ï¿½ 2019 Acid Rain Studios LLC
 *
 * This library is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Lesser General Public License as published by the Free
@@ -32,7 +32,7 @@
 
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(int /*argc*/, char** /*argv*/)
 {
     ////Set Log File Options (need to have this 2x...there is a bug)
     //dtUtil::LogFile::SetFileName("VRSimLog.html");
